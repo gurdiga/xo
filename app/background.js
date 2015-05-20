5970e252dc82d206f63d0b070829df63b052a743
Vlad GURDIGA <gurdiga@gmail.com>
2015-05-20 15:50:01 +0300

    start maximized

--- app/background.js
+++ app/background.js
@@ -2,9 +2,12 @@
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
+    /*
     'bounds': {
       'width': 1000,
       'height': 800
-    }
+    },
+    */
+    'state': 'maximized'
   });
 });
