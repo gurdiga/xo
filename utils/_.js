1b4881a629c8bb1610de29204d84ea1cd933955c
Vlad GURDIGA <gurdiga@gmail.com>
2015-05-21 17:43:48 +0300

    utils/_.js: each, isFunction

--- utils/_.js
+++ utils/_.js
@@ -2,7 +2,9 @@
 
 var _ = {};
 
+_.each = require('lodash/collection/forEach.js');
 _.merge = require('lodash/object/merge.js');
 _.isPlainObject = require('lodash/lang/isPlainObject.js');
+_.isFunction = require('lodash/lang/isFunction.js');
 
 module.exports = _;
