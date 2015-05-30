30e7bee38a69dd87d911468ece6996c91f0e8ee3
Vlad GURDIGA <gurdiga@gmail.com>
2015-05-30 18:44:10 +0300

    TextField: trying a different code organization

--- app/ui/text-field.jsx
+++ app/ui/text-field.jsx
@@ -1,54 +1,54 @@
 'use strict';
 
-var Styled = require('mixins/styled.js');
-var Editable = require('mixins/editable.js');
-var Valuable = require('mixins/valuable.js');
-var OutlinedOnFocus = require('mixins/outlined-on-focus.js');
+var TextField = {};
+
+TextField.render = function render() {
+  return (
+    <FieldLabel label={this.props.label}>
+      <input
+        value={this.state.value}
+        {...this.makeStyled()}
+        {...this.makeEditable()}
+        {...this.makeOutlinedOnFocus()}
+      />
+    </FieldLabel>
+  );
+};
+
+var a, an;
+a = an = React.PropTypes;
+
+TextField.propTypes = {
+  label: a.string.isRequired,
+  value: a.string,
+  style: an.object
+};
+
+TextField.getInitialState = function getInitialState() {
+  return {
+    value: this.props.value
+  };
+};
+
+TextField.style = {
+  color: 'black',
+  padding: '4px',
+  font: 'bold 14px sans-serif',
+  width: '16em',
+  backgroundImage: 'url("data:image/gif;base64,R0lGODlhMgAYAIABAN3d3f///yH5BAEKAAEALAAAAAAyABgAAAIrjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyrAGBjd96zu9+D/wFCgA7")',
+  backgroundPosition: '0 -4px',
+  borderRadius: '2px',
+  border: 'none',
+  outline: 'none'
+};
+
+TextField.mixins = [
+  require('mixins/styled.js'),
+  require('mixins/editable.js'),
+  require('mixins/valuable.js'),
+  require('mixins/outlined-on-focus.js')
+];
 
 var FieldLabel = require('./field-label.jsx');
 
-var a = React.PropTypes;
-var an = a;
-
-var TextField = React.createClass({
-  mixins: [Styled, Valuable, Editable, OutlinedOnFocus],
-
-  propTypes: {
-    label: a.string.isRequired,
-    value: a.string,
-    style: an.object
-  },
-
-  getInitialState: function() {
-    return {
-      value: this.props.value
-    };
-  },
-
-  render: function() {
-    return (
-      <FieldLabel label={this.props.label}>
-        <input
-          value={this.state.value}
-          {...this.makeStyled()}
-          {...this.makeEditable()}
-          {...this.makeOutlinedOnFocus()}
-        />
-      </FieldLabel>
-    );
-  },
-
-  style: {
-    color: 'black',
-    padding: '4px',
-    font: 'bold 14px sans-serif',
-    width: '16em',
-    backgroundImage: 'url("data:image/gif;base64,R0lGODlhMgAYAIABAN3d3f///yH5BAEKAAEALAAAAAAyABgAAAIrjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyrAGBjd96zu9+D/wFCgA7")',
-    backgroundPosition: '0 -4px',
-    borderRadius: '2px',
-    border: 'none',
-    outline: 'none'
-  }
-});
-
-module.exports = TextField;
+module.exports = React.createClass(TextField);
