bc967df6e5f9132aea22c39ac156611fb140066c
Vlad GURDIGA <gurdiga@gmail.com>
2015-06-08 07:04:36 +0300

    make deps: less indirection

--- makefiles/deps/deps.mk
+++ makefiles/deps/deps.mk
@@ -1,19 +1,17 @@
 deps: \
-	install-npm-packages \
+	node_modules \
 	node_modules/utils \
 	node_modules/mixins
 
-install-npm-packages: package.json node_modules
+node_modules: package.json
+	npm prune && \
+	npm install
 
 package.json:
 	ln --force --symbolic makefiles/deps/package.json
 
-node_modules:
-	npm prune && \
-	npm install
-
-node_modules/utils:
+node_modules/utils: | node_modules
 	ln --force --symbolic ../utils --target node_modules
 
-node_modules/mixins:
+node_modules/mixins: | node_modules
 	ln --force --symbolic ../mixins --target node_modules
