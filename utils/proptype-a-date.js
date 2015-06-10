4c1b48499d2982fa69268bae1d76e68231ec0c91
Vlad GURDIGA <gurdiga@gmail.com>
2015-06-10 20:03:36 +0300

    aDate: simplify

--- utils/proptype-a-date.js
+++ utils/proptype-a-date.js
@@ -1,9 +1,6 @@
 'use strict';
 
-var a = React.PropTypes;
-
-var DATE_FORMAT = /\d{4}-\d{2}-\d{2}/;
-var TODAY = '<today>';
+var DATE_FORMAT = /\d{2}\.\d{2}\.\d{4}/;
 
 function aDate(props, propName, componentName) {
   var value = props[propName];
@@ -16,7 +13,4 @@ function aDate(props, propName, componentName) {
   }
 }
 
-module.exports = a.oneOfType([
-  aDate,
-  a.oneOf([TODAY])
-]);
+module.exports = aDate;
