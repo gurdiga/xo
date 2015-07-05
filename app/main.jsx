4d0574c8f0508df02dd23559d29b521e989ccd63
Vlad GURDIGA <gurdiga@gmail.com>
2015-07-05 19:25:28 +0300

    NewCaseButtonTest

--- app/main.jsx
+++ app/main.jsx
@@ -13,4 +13,5 @@ if (process.env.NODE_ENV === 'development') {
   require('../test/ui/FieldLabelTest.jsx');
   require('../test/ui/DateFieldTest.jsx');
   require('../test/ui/SectionTest.jsx');
+  require('../test/ui/NewCaseButtonTest.jsx');
 }
