6bfd633dfb4ad3d07dee282502ed6d9e8ebe1d94
Vlad GURDIGA <gurdiga@gmail.com>
2015-08-09 16:19:49 +0300

    TapeBrowserConsoleDotReporter: timing + housekeeping

--- test/helper.js
+++ test/helper.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
 
-  TapeBrowserConsoleDotReporter(window.tape);
+  window.TapeBrowserConsoleDotReporter(window.tape);
 
 }());
--- test/tape-browser-console-dot-reporter.js
+++ test/tape-browser-console-dot-reporter.js
@@ -9,22 +9,46 @@
   window.TapeBrowserConsoleDotReporter = TapeBrowserConsoleDotReporter;
 
   function TapeBrowserConsoleDotReporter(tape) {
-    tape
-    .createStream({objectMode: true})
-    .on('data', function(message) {
-      switch(message.type) {
-        case 'assert': count(message); log(message); printOutput(); break;
-        case 'test': break;
-        case 'end': break;
-        default: console.log(message);
-      }
-    })
-    .on('end', displayResults);
+    reportDuration(
+      tape.createStream({objectMode: true})
+      .on('data', processMessage)
+      .on('end', displayResults)
+    );
   }
 
+  function reportDuration(stream) {
+    var started = false;
+    var startTime, endTime;
+
+    stream
+    .on('data', function() {
+      if (started) return;
+
+      startTime = new Date();
+      started = true;
+    })
+    .on('end', function() {
+      endTime = new Date();
+      started = false;
+
+      var seconds = (endTime - startTime) / 1000;
+
+      console.log('test run time: ' + seconds + 's');
+    });
+  }
+
+  function processMessage(message) {
+    switch(message.type) {
+      case 'assert': count(message); log(message); printOutput(); break;
+      case 'test': break;
+      case 'end': break;
+      default: console.log('-- unknown message', message);
+    }
+  }
 
   function count(message) {
     totalCount++;
+
     if (!message.ok) failedCount++;
   }
 
@@ -40,37 +64,41 @@
   }
 
   function displayResults() {
-    console.log('total', totalCount, 'failed', failedCount);
+    console.log('total: ', totalCount);
+    console.log('failed:', failedCount);
 
     if (failureMessages.length === 0) return;
 
     console.log('\n\n');
 
-    failureMessages.forEach(function(message) {
-      console.log('assertion:', message.name);
-      console.log('operator: ', message.operator);
-      console.log('expected: ', typeof message.expected, JSON.stringify(message.expected));
-      console.log('actual:   ', typeof message.actual  , JSON.stringify(message.actual));
-      console.log('location: ', appStack(message));
-    });
+    failureMessages.forEach(printFailureMessage);
+  }
+
+  function printFailureMessage(message) {
+    console.log('assertion:', message.name);
+    console.log('operator: ', message.operator);
+    console.log('expected: ', typeof message.expected, JSON.stringify(message.expected));
+    console.log('actual:   ', typeof message.actual  , JSON.stringify(message.actual));
+    console.log('location: ', getAppStack(message));
+  }
+
+  function getAppStack(message) {
+    var stackLines = message.error.stack.split('\n').slice(1);
+    var appStackLines = stackLines.filter(isNonTapeLine).map(getPathInfo);
+
+    return appStackLines.join('\n');
   }
 
   var IS_TAPE_STACK_LINE = /lib\/tape\.js:/;
 
-  function nonTapeLine(line) {
+  function isNonTapeLine(line) {
     return !IS_TAPE_STACK_LINE.test(line);
   }
 
   var INSIDE_PARENS = /\((.+)\)/;
-  function pathInfo(line) {
+
+  function getPathInfo(line) {
     return line.match(INSIDE_PARENS)[1];
-  }
-
-  function appStack(message) {
-    var stackLines = message.error.stack.split('\n').slice(1);
-    var appStackLines = stackLines.filter(nonTapeLine).map(pathInfo);
-
-    return appStackLines.join('\n');
   }
 
 }());
