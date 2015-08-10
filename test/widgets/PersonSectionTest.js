01b5ad9885d184c980e3a65b5f58642e72a2e823
Vlad GURDIGA <gurdiga@gmail.com>
2015-08-10 18:41:26 +0300

    PersonSectionTest: final tests

--- test/widgets/PersonSectionTest.js
+++ test/widgets/PersonSectionTest.js
@@ -6,7 +6,9 @@
 
   var value = {
     'gen-persoană': PersonSection.PERSON_TYPES.INDIVIDUAL,
-    'nume': 'John DOE'
+    'nume': 'John DOE',
+    'idnp': '0123456789',
+    'data-naşterii': '01.02.1960'
   };
   var label = 'Test person section';
   var renderIntoDocument = React.addons.TestUtils.renderIntoDocument;
@@ -14,50 +16,130 @@
   var personSection = renderIntoDocument(e(PersonSection, {label: label, value: value}));
 
   test('PersonSection', function(t) {
-    var section = personSection.refs.section;
-    t.ok(section, 'renders a section');
-    t.equal(section.props.label, label, 'section has the appropriate label');
+    t.equal(getSectionLabel(), label, 'section has the appropriate label');
 
     t.test('person type field', function(t) {
-      var personTypeField = personSection.refs['gen-persoană'];
-      t.ok(personTypeField, 'is rendered');
-      t.equal(personTypeField.props.label, 'Gen persoană', 'has the appropriate label');
-      t.equal(personTypeField.props.onChange, personSection.onPersonTypeChange, 'has the appropriate onChange');
-      t.equal(personTypeField.props.isValuable, true, 'is marked as valuable');
-      t.equal(personTypeField.props.value, PersonSection.PERSON_TYPES.INDIVIDUAL, 'has the default value of “fizică”');
+      t.equal(getFieldLabel('gen-persoană'), 'Gen persoană', 'has the appropriate label');
+      t.equal(getFieldOnChangeHandler('gen-persoană'), personSection.onPersonTypeChange, 'has the appropriate onChange');
+      t.equal(getFieldValue('gen-persoană'), PersonSection.PERSON_TYPES.INDIVIDUAL, 'has the default value of “fizică”');
 
-      var optionDOMElements = personTypeField.getDOMNode().querySelectorAll('select option');
-      var optionTexts = [].map.call(optionDOMElements, function(option) {
-        return option.textContent;
-      });
+      var optionTexts = getOptionTexts();
 
       PersonSection.PERSON_TYPES.forEach(function(type) {
         t.ok(optionTexts.indexOf(type) > -1, 'has the “' + type + '” option');
       });
 
       t.end();
+
+      function getFieldLabel(id) {
+        return personSection.refs[id].props.label;
+      }
+
+      function getFieldOnChangeHandler(id) {
+        return personSection.refs[id].props.onChange;
+      }
+
+      function getFieldValue(id) {
+        return personSection.refs[id].props.value;
+      }
+
+      function getOptionTexts() {
+        var personTypeField = personSection.refs['gen-persoană'];
+        var optionDOMElements = personTypeField.getDOMNode().querySelectorAll('select option');
+
+        return [].map.call(optionDOMElements, function(option) {
+          return option.textContent;
+        });
+      }
     });
 
     t.end();
+
+    function getSectionLabel() {
+      return personSection.refs['section'].props.label;
+    }
   });
 
-  test('PersonSection when it’s “' + value['gen-persoană'] + '”', function(t) {
-    var component = renderShallowly(e(PersonSection, {label: label, value: value}));
-    var fieldComponents = component.props.children[1];
+  [{
+    personType: PersonSection.PERSON_TYPES.INDIVIDUAL,
+    fieldValues: {
+      'gen-persoană': PersonSection.PERSON_TYPES.INDIVIDUAL,
+      'nume': 'John DOE',
+      'idnp': '0123456789',
+      'data-naşterii': '01.02.1960',
+      'domiciliu': 'bd. Victoriei 5',
+      'note': 'agreabil'
+    },
+    expectedFields: [
+      ['Nume',          'TextField',      'nume'         ],
+      ['IDNP',          'TextField',      'idnp'         ],
+      ['Data naşterii', 'DateField',      'data-naşterii'],
+      ['Domiciliu',     'LargeTextField', 'domiciliu'    ],
+      ['Note',          'LargeTextField', 'note'         ],
+    ]
+  }, {
+    personType: PersonSection.PERSON_TYPES.COMPANY,
+    fieldValues: {
+      'gen-persoană': PersonSection.PERSON_TYPES.COMPANY,
+      'denumire': 'HAPPY SRL',
+      'idno': '012345',
+      'sediu': 'bd. Fericirii',
+      'persoană-de-contact': 'Ion Cutărescu'
+    },
+    expectedFields: [
+      ['Denumire',            'TextField',      'denumire'            ],
+      ['IDNO',                'TextField',      'idno'                ],
+      ['Sediu',               'LargeTextField', 'sediu'               ],
+      ['Persoană de contact', 'TextField',      'persoană-de-contact' ],
+      ['Note',                'LargeTextField', 'note'                ],
+    ]
+  }].forEach(function(testData) {
+    test('PersonSection has the appropriate fields when person type is “' + testData.personType + '”', function(t) {
+      var sectionWidget = render(e(PersonSection, {label: label, value: testData.fieldValues}));
 
-    var nameFieldComponent = fieldComponents[0];
-    var expectedProps = { isValuable: true, label: 'Nume', value: 'John DOE' };
-    t.equal(nameFieldComponent.type.name, TextField.name, 'name field component is of the appropriate type');
-    t.deepEqual(nameFieldComponent.props, expectedProps, 'name field component is passed the appropriate props');
+      testData.expectedFields.forEach(assertField(sectionWidget, testData.fieldValues, t));
 
-    t.end();
+      t.end();
+    });
   });
 
-  function renderShallowly(component) {
+  function assertField(sectionWidget, fieldValues, t) {
+    return function(field, i) {
+      var fieldWidget = getFieldWidgetAtPosition(sectionWidget, i);
+
+      var expectedLabel     = field[0];
+      var expectedTypeName  = field[1];
+      var expectedValue     = fieldValues[field[2]];
+
+      var prefix = 'field #' + (i + 1) + ' — ' + field[2] + ' — ';
+
+      t.equal(getFieldLabel(fieldWidget),     expectedLabel,    prefix + 'has the appropriate label');
+      t.equal(getFieldTypeName(fieldWidget),  expectedTypeName, prefix + 'is of the appropriate kind');
+      t.equal(getFieldValue(fieldWidget),     expectedValue,    prefix + 'is prefilled with the appropriate value');
+    };
+  }
+
+  function render(widget) {
     var renderer = React.addons.TestUtils.createRenderer();
-    renderer.render(component);
+    renderer.render(widget);
     return renderer.getRenderOutput();
   }
 
-  var TextField = window.App.Widgets.TextField;
+  function getFieldWidgetAtPosition(sectionWidget, n) {
+    var fieldWidgets = sectionWidget.props.children[1];
+    return fieldWidgets[n];
+  }
+
+  function getFieldLabel(fieldWidget) {
+    return fieldWidget.props.label;
+  }
+
+  function getFieldTypeName(fieldWidget) {
+    return fieldWidget.type.displayName;
+  }
+
+  function getFieldValue(fieldWidget) {
+    return fieldWidget.props.value;
+  }
+
 }());
