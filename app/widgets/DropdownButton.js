da38b762e6bc4933bb1f5aa6ed8f88026e120f34
Vlad GURDIGA <gurdiga@gmail.com>
2015-09-13 20:47:31 +0300

    DropdownButton: more tests passing

--- app/widgets/DropdownButton.js
+++ app/widgets/DropdownButton.js
@@ -1,10 +1,32 @@
 (function() {
   'use strict';
 
-  function DropdownButton() {
+  function DropdownButton(labelText, options) {
     var domElement = document.createElement('dropdown-button');
 
+    addToggleButton();
+    addOptionList();
+
     this.appendTo = getAppenderOf(domElement);
+
+    function addToggleButton() {
+      var button = document.createElement('button');
+      button.textContent = labelText;
+
+      domElement.appendChild(button);
+    }
+
+    function addOptionList() {
+      var optionList = document.createElement('ul');
+
+      for (var optionLabel in options) {
+        var option = document.createElement('li');
+        option.textContent = optionLabel;
+        optionList.appendChild(option);
+      }
+
+      domElement.appendChild(optionList);
+    }
   }
 
   var getAppenderOf = window.App.Utils.getAppenderOf;
--- test/widgets/DropdownButtonTest.js
+++ test/widgets/DropdownButtonTest.js
@@ -17,6 +17,35 @@
       var domElement = sandbox.querySelector('dropdown-button');
       t.ok(domElement, 'exists');
 
+      t.test('toggle button', function(t) {
+        var toggleButton = domElement.querySelector('button');
+
+        t.ok(toggleButton, 'exists');
+        t.equal(toggleButton.textContent, 'Add', 'has the appropriate label');
+
+        t.end();
+      });
+
+      t.test('option list', function(t) {
+        var optionList = domElement.querySelector('ul');
+
+        t.ok(optionList, 'exists');
+
+        var expectedOptionLabels = Object.keys(options);
+        var expectedOptionCount = expectedOptionLabels.length;
+        var actualOptions = optionList.querySelectorAll('li');
+        t.equal(actualOptions.length, expectedOptionCount, 'has the appropriate number of options');
+
+        var optionLabels = _.map(actualOptions, _.property('textContent'));
+        t.deepEqual(optionLabels, expectedOptionLabels, 'options have the appropriate labels');
+
+        // TODO
+        // test that each list item has a button inside it; with the expected textContent?
+        //
+
+        t.end();
+      });
+
       t.end();
     });
 
