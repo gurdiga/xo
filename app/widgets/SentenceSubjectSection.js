b1d30da26768ec5076a32a18cd46ec1df1648c19
Vlad GURDIGA <gurdiga@gmail.com>
2015-09-30 10:50:53 +0300

    SentenceSubjectSection: first test

---
+++ app/widgets/SentenceSubjectSection.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  function SentenceSubjectSection() {
+  }
+
+  window.App.Widgets.SentenceSubjectSection = SentenceSubjectSection;
+
+}());
---
+++ test/widgets/SentenceSubjectSectionTest.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  var SentenceSubjectSection = window.App.Widgets.SentenceSubjectSection;
+
+  tape.test('SentenceSubjectSection', function(t) {
+    t.equal(typeof SentenceSubjectSection, 'function', 'is a function');
+
+    t.end();
+  });
+
+}());
