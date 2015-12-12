1b42425c086e5db0a2a4612a4ad1b9eb88150816
Vlad GURDIGA <gurdiga@gmail.com>
2015-12-12 15:19:29 +0200

    App.Utils.createDOMElementTest
    A utility function that creates a dom element given a tag name and a
    hash of style properties.

---
+++ test/utils/createDOMElementTest.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var createDOMElement = window.App.Utils.createDOMElement;
+  var test = tape;
+
+  test('createDOMElement', function(t) {
+    t.ok(createDOMElement);
+
+    t.end();
+  });
+
+}());
