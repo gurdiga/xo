f7a0724c39d640d7a823b05296f54ef90151908f
Vlad GURDIGA <gurdiga@gmail.com>
2015-12-13 13:10:29 +0200

    making use of createDOMElement

--- app/widgets/FieldLabel.js
+++ app/widgets/FieldLabel.js
@@ -2,33 +2,45 @@
   'use strict';
 
   function FieldLabel(text, additionalStyle, childWidgets) {
-    var domElement = document.createElement('label');
-    _.extend(domElement.style, style, additionalStyle);
+    var domElement = createElement(additionalStyle);
+    var span = createSpan(text);
 
-    var span = document.createElement('span');
-    span.textContent = text;
-    _.extend(span.style, spanStyle);
     domElement.appendChild(span);
-
     appendWidgets(childWidgets).to(domElement);
 
     this.appendTo = getAppenderOf(domElement);
   }
 
-  var style = {
-    display: 'inline-block',
-    margin: '0 0 3px 5px'
-  };
+  function createElement(additionalStyle) {
+    var defaultStyle = {
+      display: 'inline-block',
+      margin: '0 0 3px 5px'
+    };
 
-  var spanStyle = {
-    color: '#555',
-    fontSize: '14px',
-    display: 'inline-block',
-    width: '11em'
-  };
+    var style = _.extend(defaultStyle, additionalStyle);
+    var domElement = createDOMElement('label', style);
+
+    return domElement;
+  }
+
+  function createSpan(text) {
+    var style = {
+      color: '#555',
+      fontSize: '14px',
+      display: 'inline-block',
+      width: '11em'
+    };
+
+    var span = createDOMElement('span', style);
+
+    span.textContent = text;
+
+    return span;
+  }
 
   var appendWidgets = window.App.Utils.appendWidgets;
   var getAppenderOf = window.App.Utils.getAppenderOf;
+  var createDOMElement = window.App.Utils.createDOMElement;
 
   window.App.Widgets.FieldLabel = FieldLabel;
 
