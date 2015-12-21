024dfd7a3c86158af3384e42da70ac66e43ae42f
Vlad GURDIGA <gurdiga@gmail.com>
2015-12-21 09:33:53 +0200

    InstitutionActivity: has a date field

--- app/widgets/InstitutionActivity.js
+++ app/widgets/InstitutionActivity.js
@@ -4,8 +4,19 @@
   function InstitutionActivity() {
     var domElement = createDOMElement('institution-activity');
 
+    addDateField(domElement);
+
     this.appendTo = getAppenderOf(domElement);
   }
+
+  function addDateField(domElement) {
+    var value = '';
+    var style = { width: '6.5em' };
+    var dateField = new DateField(value, style);
+    dateField.appendTo(domElement);
+  }
+
+  var DateField = window.App.Widgets.DateField;
 
   var getAppenderOf = window.App.Utils.getAppenderOf;
   var createDOMElement = window.App.Utils.createDOMElement;
--- test/widgets/InstitutionActivityTest.js
+++ test/widgets/InstitutionActivityTest.js
@@ -3,8 +3,27 @@
 
   var InstitutionActivity = window.App.Widgets.InstitutionActivity;
 
+  var sandbox = document.createElement('div');
+  var institutionActivity = new InstitutionActivity();
+  institutionActivity.appendTo(sandbox);
+
   tape('InstitutionActivity', function(t) {
-    t.ok(InstitutionActivity, 'exists');
+    var domElement = sandbox.firstChild;
+
+    t.ok(domElement, 'exists');
+    t.equal(domElement.tagName, 'INSTITUTION-ACTIVITY', 'has the appropriate tag name');
+
+    t.test('date field', function(t) {
+      var dateField = domElement.firstChild;
+
+      t.ok(dateField, 'exists');
+      t.equal(dateField.tagName, 'DATE-FIELD', 'is an unlabeled data field');
+
+      var input = dateField.querySelector('input');
+      t.equal(input.style.width, '6.5em', 'the  date field is a bit narrower than labeled fields');
+
+      t.end();
+    });
 
     t.end();
   });
