013443020c3f90a5ffc1f6ecf6d6fc48f1ea7644
Vlad GURDIGA <gurdiga@gmail.com>
2015-12-26 17:10:09 +0200

    CreateWritButton: styling

--- app/widgets/CreateWritButton.js
+++ app/widgets/CreateWritButton.js
@@ -8,13 +8,25 @@
   }
 
   function createElement() {
-    var domElement = document.createElement('button');
-    domElement.textContent = 'Încheiere';
+    var style = {
+      color: 'rgb(119, 119, 119)',
+      font: 'bold 14px sans-serif',
+      padding: '5px 14px',
+      background: 'transparent',
+      border: '1px solid silver',
+      borderRadius: '5px'
+    };
+
+    var domElement = createDOMElement('button', style);
+
+    domElement.textContent = 'încheiere';
     domElement.setAttribute('widget', 'CreateWritButton');
+
     return domElement;
   }
 
   var getAppenderOf = window.App.Utils.getAppenderOf;
+  var createDOMElement = window.App.Utils.createDOMElement;
 
   window.App.Widgets.CreateWritButton = CreateWritButton;
 
--- test/widgets/CreateWritButtonTest.js
+++ test/widgets/CreateWritButtonTest.js
@@ -11,7 +11,22 @@
     var domElement = sandbox.firstChild;
 
     t.equal(domElement.getAttribute('widget'), 'CreateWritButton', 'has the appropriate “widget” attribute');
-    t.equal(domElement.textContent, 'Încheiere', 'has the appropriate label');
+    t.equal(domElement.textContent, 'încheiere', 'has the appropriate label');
+
+    t.test('styling', function(t) {
+      var css = domElement.style;
+
+      t.equal(css.fontFamily, 'sans-serif', 'has sans-serif font family');
+      t.equal(css.fontSize, '14px', 'has the appropriate font size');
+      t.equal(css.fontWeight, 'bold', 'is bold');
+      t.equal(css.padding, '5px 14px', 'has a nice padding');
+      t.equal(css.border, '1px solid silver', 'has a nice border');
+      t.equal(css.borderRadius, '5px', 'has 5px-rounded corners');
+      t.equal(css.color, 'rgb(119, 119, 119)', 'has the appropriate text color');
+      t.equal(css.background, 'transparent', 'has transparent background');
+
+      t.end();
+    });
 
     t.end();
   });
