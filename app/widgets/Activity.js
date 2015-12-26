46e80656eadd35d4163c37dee6e5d5bc7640c8e6
Vlad GURDIGA <gurdiga@gmail.com>
2015-12-26 18:15:26 +0200

    Activity: styling and the date field

--- app/widgets/Activity.js
+++ app/widgets/Activity.js
@@ -4,17 +4,30 @@
   function Activity() {
     var domElement = createElement();
 
+    appendWidgets([
+      new ActivityDateField(),
+    ]).to(domElement);
+
     this.appendTo = getAppenderOf(domElement);
   }
 
   function createElement() {
-    var domElement = createDOMElement('fieldset');
+    var style = {
+      borderWidth: '0',
+      padding: '0'
+    };
+
+    var domElement = createDOMElement('fieldset', style);
     domElement.setAttribute('widget', 'Activity');
+
     return domElement;
   }
 
+  var ActivityDateField = window.App.Widgets.ActivityDateField;
+
   var getAppenderOf = window.App.Utils.getAppenderOf;
   var createDOMElement = window.App.Utils.createDOMElement;
+  var appendWidgets = window.App.Utils.appendWidgets;
 
   window.App.Widgets.Activity = Activity;
 
--- test/widgets/ActivityTest.js
+++ test/widgets/ActivityTest.js
@@ -6,12 +6,29 @@
   var activity = new Activity();
   var sandbox = document.createElement('div');
   activity.appendTo(sandbox);
+  document.body.appendChild(sandbox); // TODO: remove me
 
   tape('Activity', function(t) {
     var domElement = sandbox.firstChild;
 
     t.equal(domElement.tagName, 'FIELDSET', 'is a fieldset');
     t.equal(domElement.getAttribute('widget'), 'Activity', 'is an “Activity” widget');
+
+    t.test('styling', function(t) {
+      var css = domElement.style;
+
+      t.equal(css.borderWidth, '0px', 'removes the iframe’s default border');
+      t.equal(css.padding, '0px', 'removes the iframe’s default padding');
+
+      t.end();
+    });
+
+    t.test('date field', function(t) {
+      var dateField = domElement.firstChild;
+      t.equal(dateField.tagName, 'ACTIVITY-DATE-FIELD', 'is the appropriate widget');
+
+      t.end();
+    });
 
     t.end();
   });
