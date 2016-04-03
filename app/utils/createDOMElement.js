6cbc62a08101e3f395a80d1b52ce8aed3c7c065e
Vlad GURDIGA <gurdiga@gmail.com>
2016-04-03 18:11:18 +0300

    Utils.createDOMElement: accept a hash of attributes

--- app/utils/createDOMElement.js
+++ app/utils/createDOMElement.js
@@ -1,12 +1,19 @@
 (function() {
   'use strict';
 
-  function createDOMElement(tagName, style) {
+  function createDOMElement(tagName, style, attributes) {
     var domElement = document.createElement(tagName);
 
     _.extend(domElement.style, style);
+    _.each(attributes, setAttribute(domElement));
 
     return domElement;
+  }
+
+  function setAttribute(domElement) {
+    return function(value, name) {
+      domElement.setAttribute(name, value);
+    };
   }
 
   window.App.Utils.createDOMElement = createDOMElement;
--- test/utils/createDOMElementTest.js
+++ test/utils/createDOMElementTest.js
@@ -5,13 +5,21 @@
   var test = tape;
 
   test('createDOMElement', function(t) {
-    var domElement = createDOMElement('some-component', {
+    var style = {
       color: 'green'
-    });
+    };
+
+    var attributes = {
+      widget: 'SpecialWidget'
+    };
+
+    var domElement = createDOMElement('some-component', style, attributes);
 
     t.ok(domElement instanceof HTMLElement, 'creates an HTMLElement');
     t.equal(domElement.tagName, 'SOME-COMPONENT', 'the element has the appropriate tag name');
     t.deepEqual(domElement.style.color, 'green', 'the element gets the passed in style attributes');
+    t.deepEqual(domElement.getAttribute('widget'), 'SpecialWidget',
+      'the element gets the passed in attributes');
 
     t.end();
   });
