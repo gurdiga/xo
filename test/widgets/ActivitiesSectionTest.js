bf3a3ef1c77b5baaf88550aae2e555392ea6484d
Vlad GURDIGA <gurdiga@gmail.com>
2016-04-16 11:46:34 +0300

    housekeeping

--- test/widgets/ActivitiesSectionTest.js
+++ test/widgets/ActivitiesSectionTest.js
@@ -11,15 +11,22 @@
   tape.test('ActivitiesSection', function(t) {
     var domElement = sandbox.firstChild;
 
-    t.equal(domElement.tagName, 'CASE-ACTIVITIES-SECTION', 'has the appropriate tag name');
+    t.test('structure', function(t) {
+      t.equal(domElement.tagName, 'CASE-ACTIVITIES-SECTION', 'has the appropriate tag name');
 
-    t.test('structure');
-    var css = domElement.style;
-    t.equal(css.display, 'block', 'has block display to be on its own line');
-    t.equal(css.color, additionalStyle.color, 'accepts additional CSS');
+      var labelText = domElement.querySelector('legend').textContent;
+      t.equal(labelText, 'Acţiuni procedurale', 'section has the appropriate label');
 
-    var labelText = domElement.querySelector('legend').textContent;
-    t.equal(labelText, 'Acţiuni procedurale', 'section has the appropriate label');
+      t.end();
+    });
+
+    t.test('style', function(t) {
+      var css = domElement.style;
+      t.equal(css.display, 'block', 'has block display to be on its own line');
+      t.equal(css.color, additionalStyle.color, 'accepts additional CSS');
+
+      t.end();
+    });
 
     t.test('add activity button', function(t) {
       var addActivityButton = domElement.querySelector('dropdown-button');
