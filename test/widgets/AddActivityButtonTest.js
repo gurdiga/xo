ee10a5f1bdccdd1069adc1e28b241592ad112fa3
Vlad GURDIGA <gurdiga@gmail.com>
2016-04-19 10:40:11 +0300

    housekeeping

--- test/widgets/AddActivityButtonTest.js
+++ test/widgets/AddActivityButtonTest.js
@@ -11,22 +11,27 @@
     addActivityButton.appendTo(sandbox);
 
     var domElement = sandbox.firstChild;
+    var optionButtons = _.toArray(domElement.querySelectorAll('dropdown-button>div>button'));
 
     t.test('DOM structure', function(t) {
       t.equal(domElement.tagName, 'DROPDOWN-BUTTON', 'is implemented with a DropdownButton');
 
       var toggleButton = domElement.firstChild;
       t.equal(toggleButton.textContent, 'adaugă acţiune', 'has the appropriate label');
-
-      var optionButtons = _.toArray(domElement.querySelectorAll('dropdown-button>div>button'));
       t.equal(optionButtons.length, activities.length, 'has one option button for every activity');
 
       var optionButtonLabels = optionButtons.map(_.property('textContent'));
       t.deepEqual(optionButtonLabels, ['Intentarea'], 'has the action descriptions as labels for action buttons');
 
+      t.end();
+    });
+
+    t.test('behavior', function(t) {
       optionButtons[0].click();
-      t.equal(activityAdderMock.calls.length, 1, 'clicking an option calls the activityAdderMock');
-      t.deepEqual(activityAdderMock.calls[0].args, [activities[0]], '...with the corresponding activity instance');
+
+      t.deepEqual(activityAdderMock.calls[0].args, [activities[0]],
+        'clicking an option calls the activityAdderMock with the corresponding activity');
+      t.equal(activityAdderMock.calls.length, 1, 'clicking an option calls the activityAdderMock once');
 
       t.end();
     });
