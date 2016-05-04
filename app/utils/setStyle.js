5bd1b458b54416f48cae394508fcf733f98504d4
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-04 09:59:59 +0300

    setStyle expects the first argument to be a DOM element

--- app/utils/setStyle.js
+++ app/utils/setStyle.js
@@ -2,8 +2,16 @@
   'use strict';
 
   function setStyle(domElement, style) {
+    assert(isDOMElement(domElement), 'setStyle expects the first argument to be a DOM element');
+
     _.extend(domElement.style, style);
   }
+
+  function isDOMElement(value) {
+    return value && value.nodeType;
+  }
+
+  var assert = window.App.Utils.assert;
 
   window.App.Utils.setStyle = setStyle;
 
--- test/utils/setStyleTest.js
+++ test/utils/setStyleTest.js
@@ -11,6 +11,17 @@
 
     t.equal(domElement.style.color, style.color, 'sets the given style on the DOM element');
 
+    t.test('input validation', function(t) {
+      t.throws(function() {
+        setStyle(42);
+      },
+        /setStyle expects the first argument to be a DOM element/,
+        'throws a meaningful error when the first argument is not a DOM element'
+      );
+
+      t.end();
+    });
+
     t.end();
   });
 
