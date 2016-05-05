b99e0760bd48a360831d328af4715f045d7ae9f2
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-05 10:04:37 +0300

    OptionList: test housekeeping

--- test/widgets/OptionListTest.js
+++ test/widgets/OptionListTest.js
@@ -4,11 +4,11 @@
   tape('OptionList', function(t) {
     var sandbox = document.createElement('div');
 
-    var handler1 = createSpy();
-    var handler2 = createSpy();
+    var optionHandler1 = createSpy();
+    var optionHandler2 = createSpy();
     var options = {
-      'label1': handler1,
-      'label2': handler2
+      'label1': optionHandler1,
+      'label2': optionHandler2
     };
 
     var optionList = new OptionList(options);
@@ -16,8 +16,8 @@
     optionList.appendTo(sandbox);
 
     var domElement = sandbox.firstChild;
-    var option1 = domElement.children[0];
-    var option2 = domElement.children[1];
+    var optionButton1 = domElement.children[0];
+    var optionButton2 = domElement.children[1];
 
     var OPTION_BUTTON_HOVER_BACKGROUND = OptionList.HOVER_OPTION_BUTTON_STYLE.backgroundColor;
     var OPTION_BUTTON_INITIAL_BACKGROUND = OptionList.INITIAL_OPTION_BUTTON_STYLE.backgroundColor;
@@ -40,7 +40,7 @@
       t.equal(style.boxShadow, 'rgba(0, 0, 0, 0.298039) 1px 1px 3px', 'has a nice shadow');
 
       t.test('option buttons', function(t) {
-        var style = option1.style;
+        var style = optionButton1.style;
 
         t.equal(style.padding, '5px 10px', 'has nice padding to increase clickable area');
         t.equal(style.borderWidth, '0px', 'removes the border off option buttons');
@@ -49,12 +49,12 @@
         t.equal(style.textAlign, 'left', 'aligns button labels left');
         t.equal(style.fontSize, '13px', 'has a nice large font size');
 
-        option1.dispatchEvent(new Event('mouseenter'));
-        t.equal(option1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+        optionButton1.dispatchEvent(new Event('mouseenter'));
+        t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
           'options backgroun change to gray on mouseenter');
 
-        option1.dispatchEvent(new Event('mouseleave'));
-        t.equal(option1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+        optionButton1.dispatchEvent(new Event('mouseleave'));
+        t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
           'options backgroun change back to normal on on mouseleave');
 
         t.end();
@@ -64,49 +64,49 @@
     });
 
     t.test('behavior', function(t) {
-      option1.click();
-      option1.click();
-      t.deepEqual(handler1.calls.length, 2, 'clicking on an option calls its corresponding handler');
+      optionButton1.click();
+      optionButton1.click();
+      t.deepEqual(optionHandler1.calls.length, 2, 'clicking on an option calls its corresponding handler');
 
-      option2.click();
-      t.deepEqual(handler2.calls.length, 1, 'clicking on an option calls its corresponding handler');
+      optionButton2.click();
+      t.deepEqual(optionHandler2.calls.length, 1, 'clicking on an option calls its corresponding handler');
 
       t.end();
     });
 
     t.test('selecting previous and next', function(t) {
       optionList.selectNext();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
         'selectNext selects the first option initially');
 
       optionList.selectNext();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'selectNext selects the option next to the currently selected one');
-      t.equal(option2.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+      t.equal(optionButton2.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
         'selectNext selects the option next to the currently selected one');
 
       optionList.selectNext();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
         'when the last option is selected selectNext goes around to the first');
-      t.equal(option2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'when the last option is selected selectNext unselects it');
 
       optionList.selectPrevious();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'when the first option is selected selectPrevious unselects it');
-      t.equal(option2.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+      t.equal(optionButton2.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
         'when the first option is selected selectPrevious goess around to the last');
 
       optionList.selectPrevious();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_HOVER_BACKGROUND,
         'selectPrevious selects the option that is above the currently selected one');
-      t.equal(option2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'selectPrevious unselects the option that was selected');
 
       optionList.hide();
-      t.equal(option1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton1.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'hiding the list unselects all the options');
-      t.equal(option2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
+      t.equal(optionButton2.style.backgroundColor, OPTION_BUTTON_INITIAL_BACKGROUND,
         'hiding the list unselects all the options');
 
       t.end();
