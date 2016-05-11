1fcadda9faa36652959639ba9c01e3907cae767b
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-11 10:38:38 +0300

    TodoList: has items

--- app/widgets/TodoList.js
+++ app/widgets/TodoList.js
@@ -1,14 +1,28 @@
 (function() {
   'use strict';
 
-  function TodoList() {
+  function TodoList(itemData) {
     var domElement = createElement();
+
+    addItems(itemData, domElement);
 
     this.appendTo = getAppenderOf(domElement);
   }
 
   function createElement() {
     return createDOMElement('ul');
+  }
+
+  function addItems(itemData, domElement) {
+    itemData.forEach(function(item) {
+      domElement.appendChild(createItemElement(item));
+    });
+  }
+
+  function createItemElement(item) {
+    var itemElement = createDOMElement('li');
+    itemElement.textContent = item.text;
+    return itemElement;
   }
 
   var createDOMElement = window.App.Utils.createDOMElement;
--- test/widgets/TodoListTest.js
+++ test/widgets/TodoListTest.js
@@ -2,14 +2,24 @@
   'use strict';
 
   tape('TodoList', function(t) {
-    var todoList = new TodoList();
+    var itemData = [{
+      text: 'item one'
+    }];
+    var todoList = new TodoList(itemData);
     var sandbox = createDOMElement('sandbox');
     todoList.appendTo(sandbox);
 
     var domElement = sandbox.firstChild;
+    document.body.appendChild(sandbox);
 
     t.test('DOM structure', function(t) {
-      t.equal(domElement.tagName, 'UL', 'it’s an unordered list');
+      t.equal(domElement.tagName, 'UL', 'it’s a <ul>');
+
+      var itemElements = _.toArray(domElement.querySelectorAll('ul>li'));
+
+      t.equal(itemElements.length, itemData.length, 'renders items as <li>s');
+      t.deepEqual(itemElements.map(_.property('textContent')), [itemData[0].text],
+        'items have the appropriate text');
 
       t.end();
     });
