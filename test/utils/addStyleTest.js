ca74dd51963141d300f21316bd36a3ba3b50ce65
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-19 10:42:16 +0300

    switch to Mocha: addStyleTest.js

--- test/utils/addStyleTest.js
+++ test/utils/addStyleTest.js
@@ -1,38 +1,38 @@
-(function() {
+describe('addStyle', function() {
   'use strict';
 
-  tape('addStyle', function(t) {
-    var domElement = createDOMElement('div');
-    var style = {
+  var domElement, style;
+
+  beforeEach(function() {
+    domElement = createDOMElement('div');
+    style = {
       'color': 'red'
     };
+  });
 
+  it('works for the happy path', function() {
     addStyle(domElement, style);
 
-    t.equal(domElement.style.color, style.color, 'sets the given style on the DOM element');
+    assert.equal(domElement.style.color, style.color, 'sets the given style on the DOM element');
+  });
 
-    t.test('input validation', function(t) {
-      t.throws(function() {
-        addStyle(42);
-      },
-        /addStyle expects the first argument to be a DOM element/,
-        'throws a meaningful error when the first argument is not a DOM element'
-      );
+  it('throws meaningful error messages', function() {
+    assert.throws(function() {
+      addStyle(42);
+    },
+      /addStyle expects the first argument to be a DOM element/,
+      'throws a meaningful error when the first argument is not a DOM element'
+    );
 
-      t.throws(function() {
-        addStyle(createDOMElement('div'), 42);
-      },
-        /addStyle expects the second argument to be a hash/,
-        'throws a meaningful error when the first argument is not a hash'
-      );
-
-      t.end();
-    });
-
-    t.end();
+    assert.throws(function() {
+      addStyle(createDOMElement('div'), 42);
+    },
+      /addStyle expects the second argument to be a hash/,
+      'throws a meaningful error when the first argument is not a hash'
+    );
   });
 
   var addStyle = window.App.Utils.addStyle;
   var createDOMElement = window.App.Utils.createDOMElement;
 
-}());
+});
