cb6ad78edb2826b21772948ed2ee05e426856f2d
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-21 13:25:44 +0300

    housekeeping

--- app/utils/addStyle.js
+++ app/utils/addStyle.js
@@ -2,14 +2,10 @@
   'use strict';
 
   function addStyle(domElement, style) {
-    assert(isDOMElement(domElement), 'addStyle expects the first argument to be a DOM element');
+    assert(_.isElement(domElement), 'addStyle expects the first argument to be a DOM element');
     assert(_.isPlainObject(style), 'addStyle expects the second argument to be a hash');
 
     _.extend(domElement.style, style);
-  }
-
-  function isDOMElement(value) {
-    return value && value.nodeType;
   }
 
   var assert = window.App.Utils.assert;
--- app/utils/hideOnEscapeOrOutsideClick.js
+++ app/utils/hideOnEscapeOrOutsideClick.js
@@ -2,17 +2,13 @@
   'use strict';
 
   function hideOnEscapeOrOutsideClick(thing) {
-    assert(isDOMElement(thing) || hasHideMethod(thing),
+    assert(_.isElement(thing) || hasHideMethod(thing),
       'hideOnEscapeOrOutsideClick: argument is expected to be a DOM element or a thing that has a hide method');
 
-    if (isDOMElement(thing)) thing = new HideableDOMElement(thing);
+    if (_.isElement(thing)) thing = new HideableDOMElement(thing);
 
     document.body.addEventListener('keydown', ifKey('Escape', thing.hide));
     document.body.addEventListener('click', thing.hide);
-  }
-
-  function isDOMElement(thing) {
-    return thing && thing.nodeType;
   }
 
   function hasHideMethod(thing) {
