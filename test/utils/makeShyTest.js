00a146ffe92287698be3d1d215547e64907aba3b
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-21 12:18:57 +0300

    switch to Mocha: makeShyTest.js

--- test/utils/makeShyTest.js
+++ test/utils/makeShyTest.js
@@ -1,34 +1,39 @@
-(function() {
+describe('makeShy', function() {
   'use strict';
 
   var makeShy = window.App.Utils.makeShy;
-  var domElement = document.createElement('div');
-  domElement.style.transition = 'padding 0.8s linear';
 
-  makeShy(domElement);
+  var domElement;
 
-  tape('makeShy', function(t) {
-    t.equal(domElement.getAttribute('shy'), '', 'adds the boolean “shy” attribute for inspectability');
-    t.equal(domElement.style.opacity, '0.3', 'sets the element’s opacity to 0.3');
+  beforeEach(function() {
+    domElement = document.createElement('div');
+    domElement.style.transition = 'padding 0.8s linear';
 
-    t.test('adds a transition for opacity while preserving other transitions', function(t) {
-      var css = domElement.style;
-
-      t.equal(css.transitionProperty, 'padding, opacity', 'transition property');
-      t.equal(css.transitionDuration, '0.8s, 0.5s', 'transition duration');
-      t.equal(css.transitionTimingFunction, 'linear, ease', 'transition timing function');
-      t.equal(css.transitionDelay, '0s, 0.1s', 'transition delay');
-
-      t.end();
-    });
-
-    domElement.dispatchEvent(new Event('mouseenter'));
-    t.equal(domElement.style.opacity, '1', 'sets the element’s opacity to 1 on hover');
-
-    domElement.dispatchEvent(new Event('mouseleave'));
-    t.equal(domElement.style.opacity, '0.3', 'sets the element’s opacity back to initial on mouse leave');
-
-    t.end();
+    makeShy(domElement);
   });
 
-}());
+  it('chainges the given DOM element appropriatelly', function() {
+    assert.equal(domElement.getAttribute('shy'), '', 'adds the boolean “shy” attribute for inspectability');
+    assert.equal(domElement.style.opacity, '0.3', 'sets the element’s opacity to 0.3');
+  });
+
+  it('adds a transition for opacity while preserving other transitions', function() {
+    var css = domElement.style;
+
+    assert.equal(css.transitionProperty, 'padding, opacity', 'transition property');
+    assert.equal(css.transitionDuration, '0.8s, 0.5s', 'transition duration');
+    assert.equal(css.transitionTimingFunction, 'linear, ease', 'transition timing function');
+    assert.equal(css.transitionDelay, '0s, 0.1s', 'transition delay');
+  });
+
+  it('makes the element react to DOM event', function() {
+    domElement.dispatchEvent(new Event('mouseenter'));
+    assert.equal(domElement.style.opacity, '1', 'sets the element’s opacity to 1 on hover');
+
+    domElement.dispatchEvent(new Event('mouseleave'));
+    assert.equal(domElement.style.opacity, '0.3', 'sets the element’s opacity back to initial on mouse leave');
+  });
+
+  var assert = window.TestHelpers.assert;
+
+});
