a73d451701c649fef6b3f6c2a086b52e879a6f29
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:27:58 +0300

    switch to Mocha: createEnumArrayTest

--- test/utils/createEnumArrayTest.js
+++ test/utils/createEnumArrayTest.js
@@ -1,9 +1,9 @@
-(function() {
+describe('createEnumArray', function() {
   'use strict';
 
   var createEnumArray = window.App.Utils.createEnumArray;
 
-  tape.test('createEnumArray', function(t) {
+  it('works', function() {
     var hash = {
       K1: 'v1',
       K2: 'v2',
@@ -12,15 +12,14 @@
 
     var ENUM = createEnumArray(hash);
 
-    t.ok(ENUM instanceof Array, 'returns an array');
-    t.deepEqual(ENUM, _.values(hash), 'the returned array contains values from the given hash');
+    assert(ENUM instanceof Array, 'returns an array');
+    assert.deepEqual(ENUM, _.values(hash), 'the returned array contains values from the given hash');
 
-    t.equal(ENUM.K1, 'v1', 'the array gets properties as per hash');
-    t.equal(ENUM.K2, 'v2', 'the array gets properties as per hash');
-    t.equal(ENUM.K3, 'v3', 'the array gets properties as per hash');
-    t.deepEqual(Object.keys(ENUM), ['0', '1', '2'], 'the additional properties are not enumerable');
-
-    t.end();
+    assert.equal(ENUM.K1, 'v1', 'the array gets properties as per hash');
+    assert.equal(ENUM.K2, 'v2', 'the array gets properties as per hash');
+    assert.equal(ENUM.K3, 'v3', 'the array gets properties as per hash');
+    assert.deepEqual(Object.keys(ENUM), ['0', '1', '2'], 'the additional properties are not enumerable');
   });
 
-}());
+  var assert = window.TestHelpers.assert;
+});
