181f8c4c88a00da1f7a53a5d52e8174999e0d7f0
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:16:54 +0300

    switch to Mocha: addHoverEffect

--- test/utils/addHoverEffectTest.js
+++ test/utils/addHoverEffectTest.js
@@ -1,32 +1,40 @@
-(function() {
+describe('addHoverEffect', function() {
   'use strict';
 
-  tape('addHoverEffect', function(t) {
-    var initialStyle = {
+  var addHoverEffect = window.App.Utils.addHoverEffect;
+
+  var domElement, initialStyle, hoverStyle;
+
+  beforeEach(function() {
+    initialStyle = {
       'color': 'blue'
     };
 
-    var hoverStyle = {
+    hoverStyle = {
       'color': 'red'
     };
 
-    var domElement = createDOMElement('div', initialStyle);
+    domElement = createDOMElement('div', initialStyle);
 
     addHoverEffect(domElement, hoverStyle);
-
-    domElement.dispatchEvent(new Event('mouseenter'));
-    t.equal(domElement.style.color, hoverStyle.color, 'applies the given style on mouseenter');
-
-    domElement.dispatchEvent(new Event('mouseleave'));
-    t.equal(domElement.style.color, initialStyle.color, 'returns to the initial style on mouseleave');
-
-    t.equal(domElement.hasAttribute('has-on-hover-effect'), true,
-      'marks the element as having on hover effect');
-
-    t.end();
   });
 
-  var addHoverEffect = window.App.Utils.addHoverEffect;
+  it('works', function() {
+    domElement.dispatchEvent(new Event('mouseenter'));
+    assert.equal(domElement.style.color, hoverStyle.color, 'applies the given style on mouseenter');
+
+    domElement.dispatchEvent(new Event('mouseleave'));
+    assert.equal(domElement.style.color, initialStyle.color, 'returns to the initial style on mouseleave');
+
+    assert.equal(domElement.hasAttribute('has-on-hover-effect'), true,
+      'marks the element as having on hover effect');
+  });
+
+  it('validates input', function() {
+    // TODO
+  });
+
   var createDOMElement = window.App.Utils.createDOMElement;
 
-}());
+  var assert = window.TestHelpers.assert;
+});
