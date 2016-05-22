5b0b6da89c409c33a9a6794da4c568a9138f9816
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:30:53 +0300

    switch to Mocha: makeTextUselectableTest

--- test/utils/makeTextUselectableTest.js
+++ test/utils/makeTextUselectableTest.js
@@ -1,21 +1,27 @@
-(function() {
+describe('makeTextUselectable', function() {
   'use strict';
 
   var makeTextUselectable = window.App.Utils.makeTextUselectable;
-  var test = tape;
 
-  var domElement = document.createElement('button');
+  var domElement;
 
-  test('makeTextUselectable', function(t) {
-    makeTextUselectable(domElement);
-
-    t.equal(domElement.style['-webkit-touch-callout'], 'none', 'Webkit touch covered');
-    t.equal(domElement.style['-webkit-user-select'], 'none', 'Webkit covered');
-    t.equal(domElement.style['-moz-user-select'], 'none', 'Mozilla covered');
-    t.equal(domElement.style['-ms-user-select'], 'none', 'IE covered');
-    t.equal(domElement.style['user-select'], 'none', 'W3C covered');
-
-    t.end();
+  beforeEach(function() {
+    domElement = document.createElement('button');
   });
 
-}());
+  it('works', function() {
+    makeTextUselectable(domElement);
+
+    assert.equal(domElement.style['-webkit-touch-callout'], 'none', 'Webkit touch covered');
+    assert.equal(domElement.style['-webkit-user-select'], 'none', 'Webkit covered');
+    assert.equal(domElement.style['-moz-user-select'], 'none', 'Mozilla covered');
+    assert.equal(domElement.style['-ms-user-select'], 'none', 'IE covered');
+    assert.equal(domElement.style['user-select'], 'none', 'W3C covered');
+  });
+
+  it('validates input', function() {
+    // TODO
+  });
+
+  var assert = window.TestHelpers.assert;
+});
