6b6fe011e9f0088701ca6c6a8464f1a451d14a6d
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:41:17 +0300

    switch to Mocha: createFieldTest

--- test/utils/createFieldTest.js
+++ test/utils/createFieldTest.js
@@ -1,32 +1,34 @@
-(function() {
+describe('createField', function() {
   'use strict';
 
   var createField = window.App.Utils.createField;
 
-  tape('createField', function(t) {
-    var labelText = 'First name';
-    var internalName = 'first-name';
-    var fieldValues = { 'first-name': 'John' };
+  var labelText, internalName, fieldValues, field, fieldElement;
 
-    var field = createField(LabeledTextField, labelText, internalName, fieldValues);
-    var fieldElement = getDOMElement(field);
+  beforeEach(function() {
+    labelText = 'First name';
+    internalName = 'first-name';
+    fieldValues = { 'first-name': 'John' };
 
-    t.ok(field instanceof LabeledTextField, 'creates an instance of the given field');
-    t.equal(field.internalName, internalName, 'sets the internalName as given');
-    t.equal(field.getValue(), fieldValues['first-name'], 'sets the field value as given');
-    t.equal(getLabel(fieldElement), labelText, 'sets the field label as given');
-
-    t.end();
-
-    function getDOMElement(field) {
-      var sandbox = document.createElement('div');
-      field.appendTo(sandbox);
-      return sandbox.firstChild;
-    }
+    field = createField(LabeledTextField, labelText, internalName, fieldValues);
+    fieldElement = getDOMElement(field);
   });
+
+  it('works', function() {
+    assert(field instanceof LabeledTextField, 'creates an instance of the given field');
+    assert.equal(field.internalName, internalName, 'sets the internalName as given');
+    assert.equal(field.getValue(), fieldValues['first-name'], 'sets the field value as given');
+    assert.equal(getLabel(fieldElement), labelText, 'sets the field label as given');
+  });
+
+  function getDOMElement(field) {
+    var sandbox = document.createElement('div');
+    field.appendTo(sandbox);
+    return sandbox.firstChild;
+  }
 
   var LabeledTextField = window.App.Widgets.LabeledTextField;
 
   var getLabel = window.TestHelpers.getLabel;
-
-}());
+  var assert = window.TestHelpers.assert;
+});
