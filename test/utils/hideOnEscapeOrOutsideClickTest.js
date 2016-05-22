a57a06dc7a93d7b0a45023ae99fdc9d556a08c08
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:52:44 +0300

    switch to Mocha: hideOnEscapeOrOutsideClickTest

--- test/utils/hideOnEscapeOrOutsideClickTest.js
+++ test/utils/hideOnEscapeOrOutsideClickTest.js
@@ -1,55 +1,46 @@
-(function() {
+describe('hideOnEscapeOrOutsideClick', function() {
   'use strict';
 
-  tape('hideOnEscapeOrOutsideClick', function(t) {
-    t.test('with a DOM element', function(t) {
-      var domElement = createDOMElement('div');
+  var hideOnEscapeOrOutsideClick = window.App.Utils.hideOnEscapeOrOutsideClick;
 
-      hideOnEscapeOrOutsideClick(domElement);
+  it('works with a DOM element', function() {
+    var domElement = createDOMElement('div');
 
-      domElement.style.display = 'block';
-      document.body.dispatchEvent(new Event('click'));
-      t.equal(domElement.style.display, 'none', 'hides it when clicking the body');
+    hideOnEscapeOrOutsideClick(domElement);
 
-      domElement.style.display = 'block';
-      simulateEscapeKey();
-      t.equal(domElement.style.display, 'none', 'hides it when pressing the Esc key');
+    domElement.style.display = 'block';
+    document.body.dispatchEvent(new Event('click'));
+    assert.equal(domElement.style.display, 'none', 'hides it when clicking the body');
 
-      t.end();
-    });
-
-    t.test('with a thing that has a `hide` method', function(t) {
-      var thing = { hide: createSpy() };
-
-      hideOnEscapeOrOutsideClick(thing);
-
-      document.body.dispatchEvent(new Event('click'));
-      t.equal(thing.hide.calls.length, 1, 'calls thing’s hide method when clicking the body');
-
-      simulateEscapeKey();
-      t.equal(thing.hide.calls.length, 2, 'calls thing’s hide method when pressing the Esc key');
-
-      t.end();
-    });
-
-    t.test('input validation', function(t) {
-      t.throws(function() {
-        hideOnEscapeOrOutsideClick(42);
-      },
-        /argument is expected to be a DOM element or a thing that has a hide method/,
-        'throws a meaningful error when input is invalid'
-      );
-
-      t.end();
-    });
-
-    t.end();
+    domElement.style.display = 'block';
+    simulateEscapeKey();
+    assert.equal(domElement.style.display, 'none', 'hides it when pressing the Esc key');
   });
 
-  var hideOnEscapeOrOutsideClick = window.App.Utils.hideOnEscapeOrOutsideClick;
+  it('works with a thing that has a `hide` method', function() {
+    var thing = { hide: createSpy() };
+
+    hideOnEscapeOrOutsideClick(thing);
+
+    document.body.dispatchEvent(new Event('click'));
+    assert.equal(thing.hide.calls.length, 1, 'calls thing’s hide method when clicking the body');
+
+    simulateEscapeKey();
+    assert.equal(thing.hide.calls.length, 2, 'calls thing’s hide method when pressing the Esc key');
+  });
+
+  it('validates input', function() {
+    assert.throws(function() {
+      hideOnEscapeOrOutsideClick(42);
+    },
+      /argument is expected to be a DOM element or a thing that has a hide method/,
+      'throws a meaningful error when input is invalid'
+    );
+  });
+
   var createDOMElement = window.App.Utils.createDOMElement;
 
   var simulateEscapeKey = window.TestHelpers.simulateEscapeKey;
   var createSpy = window.TestHelpers.createSpy;
-
-}());
+  var assert = window.TestHelpers.assert;
+});
