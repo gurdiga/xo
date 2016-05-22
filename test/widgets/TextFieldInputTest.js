91025948d06c58ec1f7eb381909283cbd37ee07c
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 15:11:47 +0300

    switch to Mocha: TextFieldInputTest

--- test/widgets/TextFieldInputTest.js
+++ test/widgets/TextFieldInputTest.js
@@ -1,45 +1,47 @@
-(function() {
+describe('TextFieldInput', function() {
   'use strict';
 
   var TextFieldInput = window.App.Widgets.TextFieldInput;
 
-  tape('TextFieldInput', function(t) {
-    var sandbox = document.createElement('div');
-    var fieldValue = 'some text';
+  var sandbox, fieldValue, textFieldInput, input;
 
-    var textFieldInput = new TextFieldInput(fieldValue);
+  before(function() {
+    sandbox = document.createElement('div');
+    fieldValue = 'some text';
+
+    textFieldInput = new TextFieldInput(fieldValue);
     textFieldInput.appendTo(sandbox);
 
-    var input = sandbox.querySelector('input');
-
-    t.test('behavior', function(t) {
-      t.equal(input.value, textFieldInput.getValue(), 'getValue() returns <input/>’s value');
-
-      var newValue = 'new value';
-      textFieldInput.setValue(newValue);
-      t.equal(input.value, newValue, 'setValue() sets <input/>’s value');
-
-      t.end();
-    });
-
-    t.test('focusability', function(t) {
-      document.body.appendChild(sandbox);
-
-      textFieldInput.focus();
-      t.equal(document.activeElement, input, 'it focuses the <input>');
-
-      document.body.removeChild(sandbox);
-      t.end();
-    });
-
-    t.test('outline', function(t) {
-      t.equal(input.hasAttribute('has-on-focus-effect'), true,
-        'is outlined on focus');
-
-      t.end();
-    });
-
-    t.end();
+    input = sandbox.querySelector('input');
   });
 
-}());
+  it('works', function() {
+    assert.equal(input.value, textFieldInput.getValue(), 'getValue() returns <input/>’s value');
+
+    var newValue = 'new value';
+    textFieldInput.setValue(newValue);
+    assert.equal(input.value, newValue, 'setValue() sets <input/>’s value');
+  });
+
+  describe('focusability', function() {
+    beforeEach(function() {
+      document.body.appendChild(sandbox);
+    });
+
+    it('can be focused', function() {
+      textFieldInput.focus();
+      assert.equal(document.activeElement, input, 'it focuses the <input>');
+    });
+
+    afterEach(function() {
+      document.body.removeChild(sandbox);
+    });
+  });
+
+  it('gets an outline on focus', function() {
+    assert.equal(input.hasAttribute('has-on-focus-effect'), true,
+      'is outlined on focus');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
