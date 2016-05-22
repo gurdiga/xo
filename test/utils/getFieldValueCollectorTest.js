74674b17894e0dab260ee53e855222e40bb5099d
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-22 14:47:51 +0300

    switch to Mocha: getFieldValueCollectorTest

--- test/utils/getFieldValueCollectorTest.js
+++ test/utils/getFieldValueCollectorTest.js
@@ -1,67 +1,60 @@
-(function() {
+describe('getFieldValueCollector', function() {
   'use strict';
 
   var getFieldValueCollector = window.App.Utils.getFieldValueCollector;
 
-  tape('getFieldValueCollector', function(t) {
-    var field1 = new LabeledTextField('Name', 'John Doe');
+  var field1, field2, getValue;
+
+  beforeEach(function() {
+    field1 = new LabeledTextField('Name', 'John Doe');
     field1.internalName = 'name';
 
-    var field2 = new LabeledDateField('Date of birth', '01.01.1970');
+    field2 = new LabeledDateField('Date of birth', '01.01.1970');
     field2.internalName = 'date-of-birth';
+  });
 
-    var getValue;
+  it('works with an array of fields', function() {
+    getValue = getFieldValueCollector([field1, field2]);
 
-    t.test('when input is an array', function(t) {
-      getValue = getFieldValueCollector([field1, field2]);
+    assert.deepEqual(getValue(), {
+      'name': 'John Doe',
+      'date-of-birth': '01.01.1970'
+    }, 'the returned function returns the values of the fields keyed by their internalName');
+  });
 
-      t.deepEqual(getValue(), {
-        'name': 'John Doe',
-        'date-of-birth': '01.01.1970'
-      }, 'the returned function returns the values of the fields keyed by their internalName');
+  it('works when input is a function that returns an array of fields', function() {
+    getValue = getFieldValueCollector(getFields);
 
-      t.end();
-    });
+    assert.deepEqual(getValue(), {
+      'name': 'John Doe',
+      'date-of-birth': '01.01.1970'
+    }, 'also works with a function that returns an array of fields');
 
-    t.test('when input is a function that returns an array of fields', function(t) {
-      getValue = getFieldValueCollector(getFields);
+    function getFields() {
+      return [field1, field2];
+    }
+  });
 
-      t.deepEqual(getValue(), {
-        'name': 'John Doe',
-        'date-of-birth': '01.01.1970'
-      }, 'also works with a function that returns an array of fields');
+  it('validates input', function() {
+    assert.throws(function() {
+      var getValue = getFieldValueCollector([field1, field2, {}]);
+      getValue();
+    },
+      /Every field is expected to have an internalName/,
+      'throws a meaningful error when a field doesn’t have the internalName property'
+    );
 
-      t.end();
-
-      function getFields() {
-        return [field1, field2];
-      }
-    });
-
-    t.test('input validation', function(t) {
-      t.throws(function() {
-        var getValue = getFieldValueCollector([field1, field2, {}]);
-        getValue();
-      },
-        /Every field is expected to have an internalName/,
-        'throws a meaningful error when a field doesn’t have the internalName property'
-      );
-
-      t.throws(function() {
-        var getValue = getFieldValueCollector([field1, field2, { internalName: 'yes' }]);
-        getValue();
-      },
-        /Every field is expected to have a getValue method/,
-        'throws a meaningful error when a field doesn’t have the internalName property'
-      );
-
-      t.end();
-    });
-
-    t.end();
+    assert.throws(function() {
+      var getValue = getFieldValueCollector([field1, field2, { internalName: 'yes' }]);
+      getValue();
+    },
+      /Every field is expected to have a getValue method/,
+      'throws a meaningful error when a field doesn’t have the internalName property'
+    );
   });
 
   var LabeledTextField = window.App.Widgets.LabeledTextField;
   var LabeledDateField = window.App.Widgets.LabeledDateField;
 
-}());
+  var assert = window.TestHelpers.assert;
+});
