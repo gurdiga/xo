41afef9ea2f4112e1940688a4136e90713851950
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-23 19:06:12 +0300

    switch to Mocha: DateFieldTest

--- test/widgets/DateFieldTest.js
+++ test/widgets/DateFieldTest.js
@@ -1,65 +1,60 @@
-(function() {
+describe('DateField', function() {
   'use strict';
 
   var DateField = window.App.Widgets.DateField;
 
-  tape('DateField', function(t) {
-    var sandbox = document.createElement('div');
-    var defaultFieldValue = 'some date';
-    var additionalStyle = {
+  var sandbox, defaultFieldValue, additionalStyle, dateField, domElement, input;
+
+  beforeEach(function() {
+    sandbox = document.createElement('div');
+    defaultFieldValue = 'some date';
+    additionalStyle = {
       width: '100px'
     };
 
-    var dateField = new DateField(defaultFieldValue, additionalStyle);
+    dateField = new DateField(defaultFieldValue, additionalStyle);
     dateField.appendTo(sandbox);
 
-    var domElement = sandbox.firstChild;
-    t.equal(domElement.tagName, 'DATE-FIELD', 'has the appropriate tag name');
+    domElement = sandbox.firstChild;
+    assert.equal(domElement.tagName, 'DATE-FIELD', 'has the appropriate tag name');
 
-    var input = domElement.querySelector('input');
+    input = domElement.querySelector('input');
+  });
 
-    t.test('input and its value', function(t) {
-      t.ok(input, 'exists');
-      t.equal(input.value, defaultFieldValue, 'has the value as passed into constructor');
-      t.equal(input.style.width, additionalStyle.width, 'accepts the additional style passed into constructor');
+  it('accepts and tells its value', function() {
+    assert.equal(input.value, defaultFieldValue, 'has the value as passed into constructor');
+    assert.equal(input.style.width, additionalStyle.width, 'accepts the additional style passed into constructor');
 
-      var newValue = 'some other value';
-      input.value = newValue;
-      t.equal(dateField.getValue(), newValue, '#getValue() returns input’s value');
+    var newValue = 'some other value';
+    input.value = newValue;
+    assert.equal(dateField.getValue(), newValue, '#getValue() returns input’s value');
+  });
 
-      t.end();
-    });
+  it('has a date picker button', function() {
+    var button = domElement.querySelector('button');
+    assert.equal(button.title, 'Deschide calendarul', 'has the appropriate tooltip');
 
-    t.test('date picker button', function(t) {
-      var button = domElement.querySelector('button');
-      t.equal(button.title, 'Deschide calendarul', 'has the appropriate tooltip');
+    var datePickerElement;
 
-      var datePickerElement;
+    button.click();
+    datePickerElement = domElement.querySelector(DateFieldInput.DATE_PICKER_SELECTOR);
+    assert(datePickerElement, 'displays the date picker on click');
 
-      button.click();
-      datePickerElement = domElement.querySelector(DateFieldInput.DATE_PICKER_SELECTOR);
-      t.ok(datePickerElement, 'displays the date picker on click');
+    button.click();
+    datePickerElement = domElement.querySelector(DateFieldInput.DATE_PICKER_SELECTOR);
+    assert(!datePickerElement, 'hides the date picker clicking the second time');
+  });
 
-      button.click();
-      datePickerElement = domElement.querySelector(DateFieldInput.DATE_PICKER_SELECTOR);
-      t.ok(!datePickerElement, 'hides the date picker clicking the second time');
+  it('is focusable', function() {
+    document.body.appendChild(sandbox);
 
-      t.end();
-    });
+    dateField.focus();
+    assert.equal(document.activeElement, input, 'focuses its <input>');
 
-    t.test('focusability', function(t) {
-      document.body.appendChild(sandbox);
-
-      dateField.focus();
-      t.equal(document.activeElement, input, 'focuses its <input>');
-
-      document.body.removeChild(sandbox);
-      t.end();
-    });
-
-    t.end();
+    document.body.removeChild(sandbox);
   });
 
   var DateFieldInput = window.App.Widgets.DateFieldInput;
 
-}());
+  var assert = window.TestHelpers.assert;
+});
