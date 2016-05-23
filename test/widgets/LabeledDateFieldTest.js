9d5df3d0f1d8b7a62e7d6011b71ec7778ed9086b
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-23 19:02:32 +0300

    switch to Mocha: LabeledDateFieldTest

--- test/widgets/LabeledDateFieldTest.js
+++ test/widgets/LabeledDateFieldTest.js
@@ -1,56 +1,52 @@
-(function() {
+describe('LabeledDateField', function() {
   'use strict';
 
   var LabeledDateField = window.App.Widgets.LabeledDateField;
 
-  tape('LabeledDateField', function(t) {
-    var sandbox = document.createElement('div');
+  var sandbox, labelText, fieldValue, labeledDateField, domElement, label, input;
 
-    var labelText = 'My LabeledDateField component';
-    var fieldValue = '22.03.2015';
+  beforeEach(function() {
+    sandbox = document.createElement('div');
 
-    var labeledDateField = new LabeledDateField(labelText, fieldValue);
+    labelText = 'My LabeledDateField component';
+    fieldValue = '22.03.2015';
+
+    labeledDateField = new LabeledDateField(labelText, fieldValue);
     labeledDateField.appendTo(sandbox);
 
-    var domElement = sandbox.firstChild;
-    var label = domElement.firstChild;
-    var input = domElement.querySelector('input');
-
-    t.equal(domElement.tagName, 'LABELED-DATE-FIELD', 'has the appropriate tag name');
-
-    t.test('label', function(t) {
-      t.equal(label.getAttribute('widget-name'), 'FieldLabel',
-        'has the appropriate “widget-name” attribute');
-      t.equal(label.textContent, labelText, 'has the appropriate text');
-
-      t.end();
-    });
-
-    t.test('field', function(t) {
-      var field = label.children[1];
-      t.equal(field.tagName, 'DATE-FIELD', 'is a <date-field/>');
-
-      t.end();
-    });
-
-    t.test('value', function(t) {
-      t.equal(input.value, fieldValue, 'has the passed in value');
-      t.equal(labeledDateField.getValue(), fieldValue, 'getValue() returns the appropriate value');
-
-      t.end();
-    });
-
-    t.test('focusability', function(t) {
-      document.body.appendChild(sandbox);
-
-      labeledDateField.focus();
-      t.equal(document.activeElement, input);
-
-      document.body.removeChild(sandbox);
-      t.end();
-    });
-
-    t.end();
+    domElement = sandbox.firstChild;
+    label = domElement.firstChild;
+    input = domElement.querySelector('input');
   });
 
-}());
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'LABELED-DATE-FIELD', 'has the appropriate tag name');
+  });
+
+  it('has the appropriate label', function() {
+    assert.equal(label.getAttribute('widget-name'), 'FieldLabel',
+      'has the appropriate “widget-name” attribute');
+    assert.equal(label.textContent, labelText, 'has the appropriate text');
+  });
+
+  it('has the appropriate field', function() {
+    var field = label.children[1];
+    assert.equal(field.tagName, 'DATE-FIELD', 'is a <date-field/>');
+  });
+
+  it('accepts and tells its value', function() {
+    assert.equal(input.value, fieldValue, 'has the passed in value');
+    assert.equal(labeledDateField.getValue(), fieldValue, 'getValue() returns the appropriate value');
+  });
+
+  it('is cousable', function() {
+    document.body.appendChild(sandbox);
+
+    labeledDateField.focus();
+    assert.equal(document.activeElement, input);
+
+    document.body.removeChild(sandbox);
+  });
+
+  var assert = window.TestHelpers.assert;
+});
