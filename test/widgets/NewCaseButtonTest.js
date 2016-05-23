64b2ed3e98484d99403bb16757634b5ceadb31f9
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-23 19:47:49 +0300

    switch to Mocha: NewCaseButtonTest

--- test/widgets/NewCaseButtonTest.js
+++ test/widgets/NewCaseButtonTest.js
@@ -1,42 +1,41 @@
-(function() {
+describe('NewCaseButton', function() {
   'use strict';
 
   var NewCaseButton = window.App.Widgets.NewCaseButton;
-  var test = tape;
 
-  var sandbox = document.createElement('div');
-  var newCaseButton = new NewCaseButton();
-  newCaseButton.onClick(onClick);
-  newCaseButton.appendTo(sandbox);
+  var sandbox, newCaseButton, onClick;
 
-  function onClick() {
-    onClick.executed = true;
-  }
+  before(function() {
+    onClick = createSpy();
 
-  test('NewCaseButton', function(t) {
+    sandbox = document.createElement('div');
+    newCaseButton = new NewCaseButton();
+    newCaseButton.onClick(onClick);
+    newCaseButton.appendTo(sandbox);
+  });
+
+  it('has the appropriate label', function() {
     var button = sandbox.querySelector('button');
-    t.equal(button.textContent, 'Procedură nouă', 'has the label “Procedură nouă”');
-    t.end();
+    assert.equal(button.textContent, 'Procedură nouă', 'has the label “Procedură nouă”');
   });
 
-  test('NewCaseButton CSS', function(t) {
+  it('has the appropriate style', function() {
     var css = sandbox.querySelector('button').style;
-    t.equal(css.padding, '0.5em 1em', 'has appropriate padding');
-    t.equal(css.fontWeight, 'bold', 'has bold text');
-    t.equal(css.fontSize, '1.5em', 'has 1.5em font size');
-    t.equal(css.backgroundColor, 'white', 'has white background color');
-    t.equal(css.border, '1px solid rgb(204, 204, 204)', 'has a thing gray border');
-    t.equal(css.borderRadius, '5px', 'has a slightly rounded corners');
-
-    t.end();
+    assert.equal(css.padding, '0.5em 1em', 'has appropriate padding');
+    assert.equal(css.fontWeight, 'bold', 'has bold text');
+    assert.equal(css.fontSize, '1.5em', 'has 1.5em font size');
+    assert.equal(css.backgroundColor, 'white', 'has white background color');
+    assert.equal(css.border, '1px solid rgb(204, 204, 204)', 'has a thing gray border');
+    assert.equal(css.borderRadius, '5px', 'has a slightly rounded corners');
   });
 
-  test('NewCaseButton behavior', function(t) {
+  it('works', function() {
     var button = sandbox.querySelector('button');
     button.click();
-    t.ok(onClick.executed, 'triggers the onClick function');
 
-    t.end();
+    assert(onClick.calls.length, 1, 'triggers the onClick function');
   });
 
-}());
+  var createSpy = window.TestHelpers.createSpy;
+  var assert = window.TestHelpers.assert;
+});
