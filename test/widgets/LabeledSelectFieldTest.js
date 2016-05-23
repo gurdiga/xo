b0e7385825d1423fa4ef26e43e79b72a557e677c
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-23 18:55:47 +0300

    switch to Mocha: LabeledSelectFieldTest

--- test/widgets/LabeledSelectFieldTest.js
+++ test/widgets/LabeledSelectFieldTest.js
@@ -1,10 +1,14 @@
-(function() {
+describe('LabeledSelectField', function() {
   'use strict';
 
-  tape('LabeledSelectField label', function(t) {
-    var sandbox = document.createElement('div');
-    var labelText = 'My LabeledSelectField component';
-    var optionValues = [
+  var LabeledSelectField = window.App.Widgets.LabeledSelectField;
+
+  var sandbox, labelText, optionValues, onChange, selectField, domElement, label, select;
+
+  beforeEach(function() {
+    sandbox = document.createElement('div');
+    labelText = 'My LabeledSelectField component';
+    optionValues = [
       'option 1',
       'option 2',
       {
@@ -16,103 +20,84 @@
       }
     ];
 
-    var onChange = createSpy();
-    var selectField = new LabeledSelectField(labelText, optionValues, optionValues[1]);
+    onChange = createSpy();
+    selectField = new LabeledSelectField(labelText, optionValues, optionValues[1]);
     selectField.onChange(onChange);
     selectField.appendTo(sandbox);
 
-    var domElement = sandbox.firstChild;
-    var label = domElement.firstChild;
-    var select = label.querySelector('select');
-
-    t.test('DOM structure', function(t) {
-      t.equal(domElement.tagName, 'LABELED-SELECT-FIELD', 'has the appropriate tag name');
-
-      t.test('label', function(t) {
-        t.equal(label.getAttribute('widget-name'), 'FieldLabel',
-          'has the appropriate “widget-name” attribute');
-        t.equal(label.firstChild.textContent, labelText, 'has the appropriate text');
-
-        t.end();
-      });
-
-      t.test('options', function(t) {
-        var options = select.querySelectorAll('option');
-        t.equal(options.length, 4, 'the option count corresponds');
-        t.equal(options[0].tagName, 'OPTION', 'the first option is an <option>');
-        t.equal(options[0].textContent, optionValues[0], 'the first <option> has the corresponding text');
-        t.equal(options[1].tagName, 'OPTION', 'the second option is an <option>');
-        t.equal(options[1].textContent, optionValues[1], 'the second <option> has the corresponding text');
-
-        var group = optionValues[2];
-        var optgroup = select.querySelector('optgroup');
-        t.equal(optgroup.label, group.optgroupLabel, 'the <optgroup> has the appropriate label');
-
-        var optgroupOptions = optgroup.querySelectorAll('option');
-        t.equal(optgroupOptions.length, 2, '<optgroup> has the appropriate number of <option>s');
-
-        t.equal(optgroupOptions[0].tagName, 'OPTION', 'the third option is an <option>');
-        t.equal(optgroupOptions[0].textContent, group.options[0],
-          'the third <option> has the corresponding text');
-        t.equal(optgroupOptions[1].tagName, 'OPTION', 'the fourth option is an <option>');
-        t.equal(optgroupOptions[1].textContent, group.options[1],
-          'the fourth <option> has the corresponding text');
-
-        t.end();
-      });
-
-      t.end();
-    });
-
-    t.test('styling', function(t) {
-      var css = select.style;
-      t.equal(css.width, '200px', 'is 200px wide too (as LabeledTextField input is)');
-      t.equal(css.fontSize, '14px', 'has 14px font size');
-      t.equal(css.position, 'absolute', 'is absolutely-positioned to allow for precise vertical alignment without bothering the <label>');
-      t.equal(css.marginTop, '-2px', 'is shifted up 2px to vertically align with the <label>');
-
-      t.end();
-    });
-
-    t.test('value', function(t) {
-      t.equal(selectField.getValue(), optionValues[1], 'its getValue() method returns the selected option');
-      select.value = optionValues[0];
-      t.equal(selectField.getValue(), optionValues[0], 'getValue() reflects the selected option');
-
-      t.test('default', function(t) {
-        var selectField = new LabeledSelectField(labelText, optionValues);
-        t.equal(selectField.getValue(), optionValues[0], 'selects the first option if none specified');
-
-        t.end();
-      });
-
-      t.end();
-    });
-
-    t.test('focusability', function(t) {
-      document.body.appendChild(sandbox);
-
-      selectField.focus();
-      t.equal(document.activeElement, select, 'focuses its <select>');
-
-      document.body.removeChild(sandbox);
-      t.end();
-    });
-
-    t.test('onChange callback', function(t) {
-      select.dispatchEvent(new Event('change'));
-
-      t.equal(onChange.calls.length, 1, 'triggers the onChange callback when changing');
-      t.deepEqual(onChange.calls[0].args, [select.value], 'onChange callback was passed the new field value');
-
-      t.end();
-    });
-
-    t.end();
+    domElement = sandbox.firstChild;
+    label = domElement.firstChild;
+    select = label.querySelector('select');
   });
 
-  var LabeledSelectField = window.App.Widgets.LabeledSelectField;
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'LABELED-SELECT-FIELD', 'has the appropriate tag name');
+  });
+
+  it('has a label', function() {
+    assert.equal(label.getAttribute('widget-name'), 'FieldLabel',
+      'has the appropriate “widget-name” attribute');
+    assert.equal(label.firstChild.textContent, labelText, 'has the appropriate text');
+  });
+
+  it('has the appropriate options', function() {
+    var options = select.querySelectorAll('option');
+    assert.equal(options.length, 4, 'the option count corresponds');
+    assert.equal(options[0].tagName, 'OPTION', 'the first option is an <option>');
+    assert.equal(options[0].textContent, optionValues[0], 'the first <option> has the corresponding text');
+    assert.equal(options[1].tagName, 'OPTION', 'the second option is an <option>');
+    assert.equal(options[1].textContent, optionValues[1], 'the second <option> has the corresponding text');
+
+    var group = optionValues[2];
+    var optgroup = select.querySelector('optgroup');
+    assert.equal(optgroup.label, group.optgroupLabel, 'the <optgroup> has the appropriate label');
+
+    var optgroupOptions = optgroup.querySelectorAll('option');
+    assert.equal(optgroupOptions.length, 2, '<optgroup> has the appropriate number of <option>s');
+
+    assert.equal(optgroupOptions[0].tagName, 'OPTION', 'the third option is an <option>');
+    assert.equal(optgroupOptions[0].textContent, group.options[0],
+      'the third <option> has the corresponding text');
+    assert.equal(optgroupOptions[1].tagName, 'OPTION', 'the fourth option is an <option>');
+    assert.equal(optgroupOptions[1].textContent, group.options[1],
+      'the fourth <option> has the corresponding text');
+  });
+
+  it('is styled appropriately', function() {
+    var css = select.style;
+    assert.equal(css.width, '200px', 'is 200px wide too (as LabeledTextField input is)');
+    assert.equal(css.fontSize, '14px', 'has 14px font size');
+    assert.equal(css.position, 'absolute', 'is absolutely-positioned to allow for precise vertical alignment without bothering the <label>');
+    assert.equal(css.marginTop, '-2px', 'is shifted up 2px to vertically align with the <label>');
+  });
+
+  it('accepts and tells its value', function() {
+    assert.equal(selectField.getValue(), optionValues[1], 'its getValue() method returns the selected option');
+    select.value = optionValues[0];
+    assert.equal(selectField.getValue(), optionValues[0], 'getValue() reflects the selected option');
+  });
+
+  it('can handle missing default value', function() {
+    var selectField = new LabeledSelectField(labelText, optionValues);
+    assert.equal(selectField.getValue(), optionValues[0], 'selects the first option if none specified');
+  });
+
+  it('is focusable', function() {
+    document.body.appendChild(sandbox);
+
+    selectField.focus();
+    assert.equal(document.activeElement, select, 'focuses its <select>');
+
+    document.body.removeChild(sandbox);
+  });
+
+  it('honors its callback', function() {
+    select.dispatchEvent(new Event('change'));
+
+    assert.equal(onChange.calls.length, 1, 'triggers the onChange callback when changing');
+    assert.deepEqual(onChange.calls[0].args, [select.value], 'onChange callback was passed the new field value');
+  });
 
   var createSpy = window.TestHelpers.createSpy;
-
-}());
+  var assert = window.TestHelpers.assert;
+});
