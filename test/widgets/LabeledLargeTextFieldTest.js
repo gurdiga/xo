ddb72d920b62aae358add4a2751472b1c0cff326
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-23 18:49:35 +0300

    switch to Mocha: LabeledLargeTextFieldTest

--- test/widgets/LabeledLargeTextFieldTest.js
+++ test/widgets/LabeledLargeTextFieldTest.js
@@ -1,80 +1,76 @@
-(function() {
+describe('LabeledLargeTextField', function() {
   'use strict';
-
-  tape('LabeledLargeTextField', function(t) {
-    var sandbox = document.createElement('div');
-    var labelText = 'My LabeledLargeTextField component';
-    var fieldValue = 'Hi!';
-
-    var largeLabeledTextField = new LabeledLargeTextField(labelText, fieldValue);
-    largeLabeledTextField.appendTo(sandbox);
-
-    var domElement = sandbox.firstChild;
-    var label = domElement.firstChild;
-    var textarea = label.children[1];
-
-    t.equal(domElement.tagName, 'LABELED-LARGE-TEXT-FIELD', 'has the appropriate tag name');
-
-    t.test('label', function(t) {
-      t.equal(label.getAttribute('widget-name'), 'FieldLabel',
-        'has the appropriate “widget-name” attribute');
-      t.equal(label.textContent, labelText, 'has the appropriate text');
-
-      t.end();
-    });
-
-    t.test('value', function(t) {
-      t.equal(textarea.value, fieldValue,
-        'the <textarea> has the value given in the LabeledLargeTextField “value” attribute');
-      t.equal(largeLabeledTextField.getValue(), fieldValue,
-        'its getValue() method returns the <textarea> value');
-
-      t.end();
-    });
-
-    t.test('focusability', function(t) {
-      document.body.appendChild(sandbox);
-
-      largeLabeledTextField.focus();
-      t.equal(document.activeElement, textarea, 'focuses its <textarea>');
-
-      document.body.removeChild(sandbox);
-      t.end();
-    });
-
-    t.test('textarea styling', function(t) {
-      var css = textarea.style;
-
-      t.equal(css.color, 'black', 'its text renders in black color');
-      t.equal(css.padding, '4px', 'has 4px of padding, as the LabeledTextField input does');
-      t.equal(css.marginLeft, '1em', 'has 1em of margin at the left, to stand out of the label vertical alignment');
-      t.equal(css.marginBottom, '5px', 'a bit of space at the bottom not to stick to the next select');
-      t.equal(css.fontWeight, 'bold', 'has bold text as the LabeledTextField input does');
-      t.equal(css.fontSize, '14px', 'has 14px font size as the LabeledTextField input does');
-      t.equal(css.fontFamily, 'sans-serif', 'has sans-serif font-family as the LabeledTextField input does');
-      t.equal(css.lineHeight, '1.75', 'has a 1.75 line height to accommodate the lines on the background');
-      t.equal(css.width, '340px', 'is 340px wide to wrap to the next line and right align with the LabeledTextField input');
-      t.equal(css.height, '5.8em', 'is 5.8em high to neatly accommodate 3 lines of text');
-      t.equal(
-        css.backgroundImage,
-        'url("data:image/gif;base64,R0lGODlhMgAYAIABAN3d3f///yH5BAEKAAEALAAAAAAyABgAAAIrjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyrAGBjd96zu9+D/wFCgA7")',
-        'has the image of a fine dotted line on the background');
-      t.equal(css.borderRadius, '2px', 'has a subtle 2px border-radius as the LabeledTextField input does');
-      t.equal(css.borderWidth, '0px',
-        'has no border because it’s replaced by the dotted line on the background image and the artificial outline');
-      t.equal(css.outlineWidth, '0px',
-        'has the built-in outline disabled because it’s replaces by the nicer artificial outline');
-      t.equal(css.resize, 'none', 'has the built-in resize control disabled because it will be made elastic');
-      t.equal(css.display, 'block', 'is block because needs to place under the label');
-
-      t.ok(textarea.hasAttribute('has-on-focus-effect'), 'is outlined on focus');
-
-      t.end();
-    });
-
-    t.end();
-  });
 
   var LabeledLargeTextField = window.App.Widgets.LabeledLargeTextField;
 
-}());
+  var sandbox, labelText, fieldValue, largeLabeledTextField, domElement, label, textarea;
+
+  beforeEach(function() {
+    sandbox = document.createElement('div');
+    labelText = 'My LabeledLargeTextField component';
+    fieldValue = 'Hi!';
+
+    largeLabeledTextField = new LabeledLargeTextField(labelText, fieldValue);
+    largeLabeledTextField.appendTo(sandbox);
+
+    domElement = sandbox.firstChild;
+    label = domElement.firstChild;
+    textarea = label.children[1];
+  });
+
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'LABELED-LARGE-TEXT-FIELD', 'has the appropriate tag name');
+  });
+
+  it('has the appropriate label', function() {
+    assert.equal(label.getAttribute('widget-name'), 'FieldLabel',
+      'has the appropriate “widget-name” attribute');
+    assert.equal(label.textContent, labelText, 'has the appropriate text');
+  });
+
+  it('accepts and tells its value', function() {
+    assert.equal(textarea.value, fieldValue,
+      'the <textarea> has the value given in the LabeledLargeTextField “value” attribute');
+    assert.equal(largeLabeledTextField.getValue(), fieldValue,
+      'its getValue() method returns the <textarea> value');
+  });
+
+  it('is focusable', function() {
+    document.body.appendChild(sandbox);
+
+    largeLabeledTextField.focus();
+    assert.equal(document.activeElement, textarea, 'focuses its <textarea>');
+
+    document.body.removeChild(sandbox);
+  });
+
+  it('is styled appropriately', function() {
+    var css = textarea.style;
+
+    assert.equal(css.color, 'black', 'its text renders in black color');
+    assert.equal(css.padding, '4px', 'has 4px of padding, as the LabeledTextField input does');
+    assert.equal(css.marginLeft, '1em', 'has 1em of margin at the left, to stand out of the label vertical alignment');
+    assert.equal(css.marginBottom, '5px', 'a bit of space at the bottom not to stick to the next select');
+    assert.equal(css.fontWeight, 'bold', 'has bold text as the LabeledTextField input does');
+    assert.equal(css.fontSize, '14px', 'has 14px font size as the LabeledTextField input does');
+    assert.equal(css.fontFamily, 'sans-serif', 'has sans-serif font-family as the LabeledTextField input does');
+    assert.equal(css.lineHeight, '1.75', 'has a 1.75 line height to accommodate the lines on the background');
+    assert.equal(css.width, '340px', 'is 340px wide to wrap to the next line and right align with the LabeledTextField input');
+    assert.equal(css.height, '5.8em', 'is 5.8em high to neatly accommodate 3 lines of text');
+    assert.equal(
+      css.backgroundImage,
+      'url("data:image/gif;base64,R0lGODlhMgAYAIABAN3d3f///yH5BAEKAAEALAAAAAAyABgAAAIrjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyrAGBjd96zu9+D/wFCgA7")',
+      'has the image of a fine dotted line on the background');
+    assert.equal(css.borderRadius, '2px', 'has a subtle 2px border-radius as the LabeledTextField input does');
+    assert.equal(css.borderWidth, '0px',
+      'has no border because it’s replaced by the dotted line on the background image and the artificial outline');
+    assert.equal(css.outlineWidth, '0px',
+      'has the built-in outline disabled because it’s replaces by the nicer artificial outline');
+    assert.equal(css.resize, 'none', 'has the built-in resize control disabled because it will be made elastic');
+    assert.equal(css.display, 'block', 'is block because needs to place under the label');
+
+    assert(textarea.hasAttribute('has-on-focus-effect'), 'is outlined on focus');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
