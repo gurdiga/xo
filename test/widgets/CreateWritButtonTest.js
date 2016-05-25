3f6cb02d714145058e4f9720f37f4926e3f4918f
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-25 11:18:54 +0300

    switch to Mocha: CreateWritButtonTest

--- test/widgets/CreateWritButtonTest.js
+++ test/widgets/CreateWritButtonTest.js
@@ -1,36 +1,36 @@
-(function() {
+describe('CreateWritButton', function() {
   'use strict';
 
   var CreateWritButton = window.App.Widgets.CreateWritButton;
 
-  var createWritButton = new CreateWritButton();
-  var sandbox = document.createElement('div');
-  createWritButton.appendTo(sandbox);
+  var createWritButton, sandbox, domElement;
 
-  tape('CreateWritButton', function(t) {
-    var domElement = sandbox.firstChild;
-
-    t.equal(domElement.getAttribute('widget-name'), 'CreateWritButton',
-      'has the appropriate “widget-name” attribute');
-    t.equal(domElement.textContent, 'încheiere', 'has the appropriate label');
-
-    t.test('styling', function(t) {
-      var css = domElement.style;
-
-      t.equal(css.fontFamily, 'sans-serif', 'has sans-serif font family');
-      t.equal(css.fontSize, '14px', 'has the appropriate font size');
-      t.equal(css.fontWeight, 'bold', 'is bold');
-      t.equal(css.padding, '5px 14px', 'has a nice padding');
-      t.equal(css.border, '1px solid silver', 'has a nice border');
-      t.equal(css.borderRadius, '5px', 'has 5px-rounded corners');
-      t.equal(css.color, 'rgb(119, 119, 119)', 'has the appropriate text color');
-      t.equal(css.background, 'transparent', 'has transparent background');
-      t.equal(css.marginTop, '5px', 'leaves some space at the top');
-
-      t.end();
-    });
-
-    t.end();
+  before(function() {
+    createWritButton = new CreateWritButton();
+    sandbox = document.createElement('div');
+    createWritButton.appendTo(sandbox);
+    domElement = sandbox.firstChild;
   });
 
-}());
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.getAttribute('widget-name'), 'CreateWritButton',
+      'has the appropriate “widget-name” attribute');
+    assert.equal(domElement.textContent, 'încheiere', 'has the appropriate label');
+  });
+
+  it('has the appropriate style', function() {
+    var css = domElement.style;
+
+    assert.equal(css.fontFamily, 'sans-serif', 'has sans-serif font family');
+    assert.equal(css.fontSize, '14px', 'has the appropriate font size');
+    assert.equal(css.fontWeight, 'bold', 'is bold');
+    assert.equal(css.padding, '5px 14px', 'has a nice padding');
+    assert.equal(css.border, '1px solid silver', 'has a nice border');
+    assert.equal(css.borderRadius, '5px', 'has 5px-rounded corners');
+    assert.equal(css.color, 'rgb(119, 119, 119)', 'has the appropriate text color');
+    assert.equal(css.background, 'transparent', 'has transparent background');
+    assert.equal(css.marginTop, '5px', 'leaves some space at the top');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
