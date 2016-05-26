382a78b62b2ebf32489a7f9ed0c2ac34666b855d
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-26 10:26:33 +0300

    switch to Mocha: ActivityDescriptionTest

--- test/widgets/ActivityDescriptionTest.js
+++ test/widgets/ActivityDescriptionTest.js
@@ -1,33 +1,33 @@
-(function() {
+describe('ActivityDescription', function() {
   'use strict';
 
   var ActivityDescription = window.App.Widgets.ActivityDescription;
 
-  var sandbox = document.createElement('div');
+  var sandbox, text, activityDescription, domElement;
 
-  var text = 'Inquiry';
-  var activityDescription = new ActivityDescription(text);
-  activityDescription.appendTo(sandbox);
+  beforeEach(function() {
+    sandbox = document.createElement('div');
 
-  tape('ActivityDescription', function(t) {
-    var domElement = sandbox.firstChild;
-
-    t.equal(domElement.tagName, 'ACTIVITY-DESCRIPTION', 'has the appropiate tag name');
-    t.equal(domElement.textContent, text, 'has the text content passed into the constructor');
-
-    t.test('styling', function(t) {
-      var css = domElement.style;
-
-      t.equal(css.display, 'inline', 'it’s expected to be on the same line with other elements');
-      t.equal(css.fontSize, '16px', 'has just a bit larger font size to stand out');
-      t.equal(css.fontWeight, 'bold', 'is bolded to stand out');
-      t.equal(css.marginLeft, '0.5em', 'keeps some space at the left');
-      t.equal(css.verticalAlign, '-1px', 'align vertically with the text in the field');
-
-      t.end();
-    });
-
-    t.end();
+    text = 'Inquiry';
+    activityDescription = new ActivityDescription(text);
+    activityDescription.appendTo(sandbox);
+    domElement = sandbox.firstChild;
   });
 
-}());
+  it('has the appropiate DOM structure', function() {
+    assert.equal(domElement.tagName, 'ACTIVITY-DESCRIPTION', 'has the appropiate tag name');
+    assert.equal(domElement.textContent, text, 'has the text content passed into the constructor');
+  });
+
+  it('has the appropiate style', function() {
+    var css = domElement.style;
+
+    assert.equal(css.display, 'inline', 'it’s expected to be on the same line with other elements');
+    assert.equal(css.fontSize, '16px', 'has just a bit larger font size to stand out');
+    assert.equal(css.fontWeight, 'bold', 'is bolded to stand out');
+    assert.equal(css.marginLeft, '0.5em', 'keeps some space at the left');
+    assert.equal(css.verticalAlign, '-1px', 'align vertically with the text in the field');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
