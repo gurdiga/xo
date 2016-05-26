dcc6a47ee9b0cf49c8f0f0e40c93d314157d732e
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-26 10:48:00 +0300

    switch to Mocha: InstitutionActivityTest

--- test/widgets/InstitutionActivityTest.js
+++ test/widgets/InstitutionActivityTest.js
@@ -1,59 +1,55 @@
-(function() {
+describe('InstitutionActivity', function() {
   'use strict';
 
   var InstitutionActivity = window.App.Widgets.InstitutionActivity;
 
-  var sandbox = document.createElement('div');
-  var institutionActivity = new InstitutionActivity();
-  institutionActivity.appendTo(sandbox);
+  var institutionActivity, domElement;
 
-  tape('InstitutionActivity', function(t) {
-    var domElement = sandbox.firstChild;
-    t.equal(domElement.getAttribute('widget-name'), 'InstitutionActivity',
-      'has the appropriate “widget-name” attribute');
-    t.equal(institutionActivity.getDescription(), 'Intentarea', 'has the appropriate description');
-
-    t.test('date field', function(t) {
-      var dateField = domElement.firstChild;
-      t.equal(dateField.tagName, 'ACTIVITY-DATE-FIELD', 'is an activity date field');
-      t.end();
-    });
-
-    t.test('description', function(t) {
-      var descriptionElement = domElement.children[1];
-
-      t.equal(descriptionElement.tagName, 'ACTIVITY-DESCRIPTION', 'has the appropriate tag name');
-      t.equal(descriptionElement.textContent, 'Intentarea', 'has the appropriate text');
-
-      t.end();
-    });
-
-    t.test('next activity options', function(t) {
-      //
-      // TODO: determine the options later.
-      //
-
-      t.deepEqual(InstitutionActivity.NEXT_ACTIVITY_OPTIONS,
-        [], 'are exposed');
-
-      t.end();
-    });
-
-    t.test('details section', function(t) {
-      var detailsSectionElement = domElement.children[2];
-      t.equal(detailsSectionElement.getAttribute('widget-name'), 'ActivityDetailsSection',
-        'has the appropriate “widget-name” attribute');
-
-      var todoList = detailsSectionElement.querySelector('[widget-name="TodoList"]');
-      t.ok(!!todoList, 'has the TODO list');
-
-      var createWritButton = detailsSectionElement.querySelector('[widget-name="CreateWritButton"]');
-      t.ok(!!createWritButton, 'has the button to creare the writ');
-
-      t.end();
-    });
-
-    t.end();
+  beforeEach(function() {
+    var sandbox = document.createElement('div');
+    institutionActivity = new InstitutionActivity();
+    institutionActivity.appendTo(sandbox);
+    domElement = sandbox.firstChild;
   });
 
-}());
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.getAttribute('widget-name'), 'InstitutionActivity',
+      'has the appropriate “widget-name” attribute');
+  });
+
+  it('has a date field', function() {
+    var dateField = domElement.firstChild;
+    assert.equal(dateField.tagName, 'ACTIVITY-DATE-FIELD', 'is an activity date field');
+  });
+
+  it('has the appropriate description', function() {
+    var descriptionElement = domElement.children[1];
+
+    assert.equal(descriptionElement.tagName, 'ACTIVITY-DESCRIPTION', 'has the appropriate tag name');
+    assert.equal(descriptionElement.textContent, 'Intentarea', 'has the appropriate text');
+    assert.equal(institutionActivity.getDescription(), 'Intentarea', 'has the appropriate description');
+  });
+
+  it('tells the next activity options', function() {
+    //
+    // TODO: determine the options later.
+    //
+
+    assert.deepEqual(InstitutionActivity.NEXT_ACTIVITY_OPTIONS,
+      [], 'are exposed');
+  });
+
+  it('has the appropriate details section', function() {
+    var detailsSectionElement = domElement.children[2];
+    assert.equal(detailsSectionElement.getAttribute('widget-name'), 'ActivityDetailsSection',
+      'has the appropriate “widget-name” attribute');
+
+    var todoList = detailsSectionElement.querySelector('[widget-name="TodoList"]');
+    assert(!!todoList, 'has the TODO list');
+
+    var createWritButton = detailsSectionElement.querySelector('[widget-name="CreateWritButton"]');
+    assert(!!createWritButton, 'has the button to creare the writ');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
