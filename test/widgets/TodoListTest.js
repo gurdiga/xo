3d2521b59b244fe8b39797a2531c7d7065576018
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-26 10:24:12 +0300

    switch to Mocha: TodoListTest

--- test/widgets/TodoListTest.js
+++ test/widgets/TodoListTest.js
@@ -1,56 +1,51 @@
-(function() {
+describe('TodoList', function() {
   'use strict';
-
-  tape('TodoList', function(t) {
-    var itemData = [{
-      text: 'item one'
-    }];
-    var todoList = new TodoList(itemData);
-    var sandbox = createDOMElement('sandbox');
-    todoList.appendTo(sandbox);
-
-    var domElement = sandbox.firstChild;
-
-    t.test('DOM structure', function(t) {
-      t.equal(domElement.tagName, 'UL', 'it’s a <ul>');
-      t.equal(domElement.getAttribute('widget-name'), 'TodoList', 'has the appropriate widget name');
-
-      var itemElements = _.toArray(domElement.querySelectorAll('ul>li>label>input[type="checkbox"]'));
-      t.equal(itemElements.length, itemData.length, 'renders items as <li>s');
-
-      var itemLabels = _.toArray(domElement.querySelectorAll('ul>li>label'));
-      var itemLabelTexts = itemLabels.map(_.property('textContent'));
-      var expectedItemLabels = itemData.map(_.property('text'));
-      t.deepEqual(itemLabelTexts, expectedItemLabels, 'items have the appropriate text');
-
-      t.end();
-    });
-
-    t.test('input validation', function(t) {
-      t.throws(function() {
-        new TodoList(42); // eslint-disable-line no-new
-      },
-        /TodoList expects first argument to be an array of objects/,
-        'throws a meaningful exception when itemData is not an array'
-      );
-
-      t.end();
-    });
-
-    t.test('style', function(t) {
-      var style = domElement.style;
-
-      t.equal(style.margin, '0px', 'has margin stripped off');
-      t.equal(style.padding, '0px', 'has padding stripped off');
-
-      t.end();
-    });
-
-    t.end();
-  });
 
   var TodoList = window.App.Widgets.TodoList;
 
-  var createDOMElement = window.App.Utils.createDOMElement;
+  var itemData, todoList, sandbox, domElement;
 
-}());
+  beforeEach(function() {
+    itemData = [{
+      text: 'item one'
+    }];
+
+    todoList = new TodoList(itemData);
+    sandbox = createDOMElement('sandbox');
+    todoList.appendTo(sandbox);
+
+    domElement = sandbox.firstChild;
+  });
+
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'UL', 'it’s a <ul>');
+    assert.equal(domElement.getAttribute('widget-name'), 'TodoList', 'has the appropriate widget name');
+
+    var itemElements = _.toArray(domElement.querySelectorAll('ul>li>label>input[type="checkbox"]'));
+    assert.equal(itemElements.length, itemData.length, 'renders items as <li>s');
+
+    var itemLabels = _.toArray(domElement.querySelectorAll('ul>li>label'));
+    var itemLabelTexts = itemLabels.map(_.property('textContent'));
+    var expectedItemLabels = itemData.map(_.property('text'));
+    assert.deepEqual(itemLabelTexts, expectedItemLabels, 'items have the appropriate text');
+  });
+
+  it('validates input', function() {
+    assert.throws(function() {
+      new TodoList(42); // eslint-disable-line no-new
+    },
+      /TodoList expects first argument to be an array of objects/,
+      'throws a meaningful exception when itemData is not an array'
+    );
+  });
+
+  it('has the appropriate style', function() {
+    var style = domElement.style;
+
+    assert.equal(style.margin, '0px', 'has margin stripped off');
+    assert.equal(style.padding, '0px', 'has padding stripped off');
+  });
+
+  var createDOMElement = window.App.Utils.createDOMElement;
+  var assert = window.TestHelpers.assert;
+});
