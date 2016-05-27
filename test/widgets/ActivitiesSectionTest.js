60fd574e0df07d4b3311535ce655251bb3868f45
Vlad GURDIGA <gurdiga@gmail.com>
2016-05-27 10:19:24 +0300

    switch to Mocha: ActivitiesSectionTest

--- test/widgets/ActivitiesSectionTest.js
+++ test/widgets/ActivitiesSectionTest.js
@@ -1,61 +1,56 @@
-(function() {
+describe('ActivitiesSection', function() {
   'use strict';
-
-  tape.test('ActivitiesSection', function(t) {
-    var sandbox = document.createElement('div');
-
-    var data = {};
-    var additionalStyle = { color: 'red' };
-    var sentenceSubjectSection = new ActivitiesSection(data, additionalStyle);
-    sentenceSubjectSection.appendTo(sandbox);
-
-    var domElement = sandbox.firstChild;
-
-    t.test('structure', function(t) {
-      t.equal(domElement.tagName, 'CASE-ACTIVITIES-SECTION', 'has the appropriate tag name');
-
-      var labelText = domElement.querySelector('legend').textContent;
-      t.equal(labelText, 'Acţiuni procedurale', 'section has the appropriate label');
-
-      t.end();
-    });
-
-    t.test('style', function(t) {
-      var style = domElement.style;
-      t.equal(style.display, 'block', 'has block display to be on its own line');
-      t.equal(style.color, additionalStyle.color, 'accepts additional CSS');
-
-      t.end();
-    });
-
-    t.test('add activity button', function(t) {
-      var addActivityButton = domElement.querySelector('dropdown-button');
-
-      var activityListContainer = addActivityButton.previousSibling;
-      t.equal(activityListContainer.getAttribute('name'), 'activity-list-container',
-        'activity list container is marked as such for inspectability');
-
-      var toggleButton = addActivityButton.querySelector('button:first-child');
-      t.equal(toggleButton.textContent, 'adaugă acţiune', 'has the appropriate label');
-
-      var options = _.toArray(addActivityButton.querySelectorAll('div>button'));
-      t.equal(options.length, 2, 'has the appropriate number of options');
-
-      var optionsLabels = options.map(_.property('textContent'));
-      t.deepEqual(optionsLabels, ['Intentarea', 'Refuz'], 'options have the appropriate labels');
-
-      t.equal(activityListContainer.children.length, 0, 'activity list is initially empty');
-      options[0].click();
-      t.equal(activityListContainer.children.length, 1, 'adds one activity');
-      t.equal(activityListContainer.children[0].getAttribute('widget-name'),
-        'InstitutionActivity', 'the added activity is an InstitutionActivity');
-
-      t.end();
-    });
-
-    t.end();
-  });
 
   var ActivitiesSection = window.App.Widgets.ActivitiesSection;
 
-}());
+  var sandbox, data, additionalStyle, sentenceSubjectSection, domElement;
+
+  beforeEach(function() {
+    sandbox = document.createElement('div');
+
+    data = {};
+    additionalStyle = { color: 'red' };
+    sentenceSubjectSection = new ActivitiesSection(data, additionalStyle);
+    sentenceSubjectSection.appendTo(sandbox);
+
+    domElement = sandbox.firstChild;
+  });
+
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'CASE-ACTIVITIES-SECTION', 'has the appropriate tag name');
+
+    var labelText = domElement.querySelector('legend').textContent;
+    assert.equal(labelText, 'Acţiuni procedurale', 'section has the appropriate label');
+  });
+
+  it('has the appropriate style', function() {
+    var style = domElement.style;
+    assert.equal(style.display, 'block', 'has block display to be on its own line');
+    assert.equal(style.color, additionalStyle.color, 'accepts additional CSS');
+  });
+
+  it('has the button to add activities', function() {
+    var addActivityButton = domElement.querySelector('dropdown-button');
+
+    var activityListContainer = addActivityButton.previousSibling;
+    assert.equal(activityListContainer.getAttribute('name'), 'activity-list-container',
+      'activity list container is marked as such for inspectability');
+
+    var toggleButton = addActivityButton.querySelector('button:first-child');
+    assert.equal(toggleButton.textContent, 'adaugă acţiune', 'has the appropriate label');
+
+    var options = _.toArray(addActivityButton.querySelectorAll('div>button'));
+    assert.equal(options.length, 2, 'has the appropriate number of options');
+
+    var optionsLabels = options.map(_.property('textContent'));
+    assert.deepEqual(optionsLabels, ['Intentarea', 'Refuz'], 'options have the appropriate labels');
+
+    assert.equal(activityListContainer.children.length, 0, 'activity list is initially empty');
+    options[0].click();
+    assert.equal(activityListContainer.children.length, 1, 'adds one activity');
+    assert.equal(activityListContainer.children[0].getAttribute('widget-name'),
+      'InstitutionActivity', 'the added activity is an InstitutionActivity');
+  });
+
+  var assert = window.TestHelpers.assert;
+});
