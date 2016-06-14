0f6cbedcc1eeda521bb94867a982bd9a22d48914
Vlad GURDIGA <gurdiga@gmail.com>
2016-06-14 12:14:08 +0300

    ActivityDetailsSection: test housekeeping

--- test/widgets/ActivityDetailsSectionTest.js
+++ test/widgets/ActivityDetailsSectionTest.js
@@ -8,12 +8,6 @@ describe('ActivityDetailsSection', function() {
   before(function() {
     activityDetailsSection = new ActivityDetailsSection();
 
-    var childWidgets = [
-      document.createElement('first-child-widget'),
-      new LabeledTextField()
-    ];
-    activityDetailsSection.setContents(childWidgets);
-
     var sandbox = document.createElement('div');
     activityDetailsSection.appendTo(sandbox);
     domElement = sandbox.firstChild;
@@ -36,11 +30,6 @@ describe('ActivityDetailsSection', function() {
     assert.equal(css.borderWidth, '0px', 'has no visible border');
   });
 
-  it('renders the given widgets', function() {
-    assert.equal(domElement.children[0].tagName, 'FIRST-CHILD-WIDGET', 'the first child is rendered');
-    assert.equal(domElement.children[1].tagName, 'LABELED-TEXT-FIELD', 'the other child is rendered');
-  });
-
   it('can be setContents()', function() {
     activityDetailsSection.setContents([
       document.createElement('another-widget')
@@ -48,8 +37,6 @@ describe('ActivityDetailsSection', function() {
     assert.equal(domElement.children.length, 1, 'has the appropriate number of child elements');
     assert.equal(domElement.children[0].tagName, 'ANOTHER-WIDGET', 'the first child is rendered');
   });
-
-  var LabeledTextField = window.App.Widgets.LabeledTextField;
 
   var assert = window.TestHelpers.assert;
 });
