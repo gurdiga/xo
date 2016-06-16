ae92e5a607afd85865b8ecea0575d5185af18970
Vlad GURDIGA <gurdiga@gmail.com>
2016-06-16 09:58:07 +0300

    TodoItem: more DOM structure

--- app/widgets/TodoItem.js
+++ app/widgets/TodoItem.js
@@ -1,10 +1,26 @@
 (function() {
   'use strict';
 
-  function TodoItem() {
+  function TodoItem(labelText) {
     var domElement = createDOMElement('li');
 
+    addLabeledCheckbox(labelText, domElement);
+
     this.appendTo = getAppenderOf(domElement);
+  }
+
+  function addLabeledCheckbox(labelText, domElement) {
+    var checkbox = createDOMElement('input');
+    checkbox.type = 'checkbox';
+
+    var labelTextContainer = createDOMElement('span');
+    labelTextContainer.textContent = labelText;
+
+    var label = createDOMElement('label');
+    label.appendChild(checkbox);
+    label.appendChild(labelTextContainer);
+
+    domElement.appendChild(label);
   }
 
   var createDOMElement = window.App.Utils.createDOMElement;
--- test/widgets/TodoItemTest.js
+++ test/widgets/TodoItemTest.js
@@ -3,15 +3,27 @@ describe('TodoItem', function() {
 
   var TodoItem = window.App.Widgets.TodoItem;
 
-  var todoItem, domElement;
+  var todoItem, domElement, labelText;
 
-  beforeEach(function() {
-    todoItem = new TodoItem();
+  before(function() {
+    labelText = 'This is the first step';
+    todoItem = new TodoItem(labelText);
     domElement = getWidgetDOMElement(todoItem);
+    document.body.appendChild(domElement); // TODO remove
   });
 
   it('has the appropriate DOM structure', function() {
     assert.equal(domElement.tagName, 'LI', 'it’s a <li>');
+
+    var implicitLabelElement = domElement.firstChild;
+    assert.equal(implicitLabelElement.tagName, 'LABEL', 'renders an implicit <label>');
+
+    var checkbox = implicitLabelElement.firstChild;
+    assert.equal(checkbox.tagName, 'INPUT', 'renders a checkbox');
+
+    var labelTextContainer = implicitLabelElement.lastChild;
+    assert.equal(labelTextContainer.tagName, 'SPAN', 'renders a span to hold the actual text label');
+    assert.equal(labelTextContainer.textContent, labelText, 'renders the given text as the text label');
   });
 
   var assert = window.TestHelpers.assert;
