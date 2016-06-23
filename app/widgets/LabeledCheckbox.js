c701a8947c5240d7fda62d87cb11d3b2fa25a9d1
Vlad GURDIGA <gurdiga@gmail.com>
2016-06-23 10:14:31 +0300

    LabeledCheckbox: has the appropriate tag name

--- app/widgets/LabeledCheckbox.js
+++ app/widgets/LabeledCheckbox.js
@@ -2,7 +2,17 @@
   'use strict';
 
   function LabeledCheckbox() {
+    var domElement = createElement();
+
+    this.appendTo = getAppenderOf(domElement);
   }
+
+  function createElement() {
+    return createDOMElement('labeled-checkbox');
+  }
+
+  var getAppenderOf = window.App.Utils.getAppenderOf;
+  var createDOMElement = window.App.Utils.createDOMElement;
 
   window.App.Widgets.LabeledCheckbox = LabeledCheckbox;
 }());
--- test/widgets/LabeledCheckboxTest.js
+++ test/widgets/LabeledCheckboxTest.js
@@ -3,9 +3,18 @@ describe('LabeledCheckbox', function() {
 
   var LabeledCheckbox = window.App.Widgets.LabeledCheckbox;
 
-  it('is extracted', function() {
-    assert.equal(typeof LabeledCheckbox, 'function', 'is a function');
+  var labeledCheckbox, labelText, domElement;
+
+  beforeEach(function() {
+    labelText = 'I do agree';
+    labeledCheckbox = new LabeledCheckbox(labelText);
+    domElement = getWidgetDOMElement(labeledCheckbox);
   });
 
+  it('has the appropriate DOM structure', function() {
+    assert.equal(domElement.tagName, 'LABELED-CHECKBOX', 'has the appropriate tag name');
+  });
+
+  var getWidgetDOMElement = window.TestHelpers.getWidgetDOMElement;
   var assert = window.TestHelpers.assert;
 });
