5adb1d0b58a4b213118c935983fa190d83ea0000
Vlad GURDIGA <gurdiga@gmail.com>
2016-08-03 12:07:37 +0300

    Section: use LabeledContainer

--- app/widgets/Section.js
+++ app/widgets/Section.js
@@ -2,54 +2,41 @@
   'use strict';
 
   function Section(labelText, childWidgets) {
-    var domElement = createElement();
+    var container = createContainer(labelText);
 
-    var legend = createLegend(labelText);
-    domElement.appendChild(legend);
+    container.appendWidgets(childWidgets);
 
-    appendWidgets(childWidgets).to(domElement);
-
-    this.appendTo = getAppenderOf(domElement);
-    this.setStyle = getStylerOf(domElement);
-
-    this.appendWidgets = function(childWidgets) {
-      appendWidgets(childWidgets).to(domElement);
-    };
+    this.appendTo = delegateTo(container, 'appendTo');
+    this.setStyle = delegateTo(container, 'setStyle');
+    this.appendWidgets = delegateTo(container, 'appendWidgets');
   }
 
-  function createElement() {
-    var style = {
-      borderWidth: '0px',
-      margin: '0',
-      padding: '10px 0 40px 5px'
-    };
+  function createContainer(labelText) {
+    var container = new LabeledContainer(labelText);
 
-    return createDOMElement('fieldset', style);
+    container.setStyle({
+      'border-width': '0px',
+      'margin': '0',
+      'padding': '10px 0 40px 5px'
+    });
+
+    container.setLabelStyle({
+      'color': 'white',
+      'background-color': '#333',
+      'width': '100%',
+      'font-weight': 'bold',
+      'font-size': '22px',
+      'font-family': 'TitleFont',
+      'margin-left': '-5px',
+      'padding': '8px 0 8px 6px'
+    });
+
+    return container;
   }
 
-  function createLegend(labelText) {
-    var style = {
-      color: 'white',
-      backgroundColor: '#333',
-      width: '100%',
-      fontWeight: 'bold',
-      fontSize: '22px',
-      fontFamily: 'TitleFont',
-      marginLeft: '-5px',
-      padding: '8px 0 8px 6px'
-    };
+  var LabeledContainer = window.App.Widgets.LabeledContainer;
 
-    var legend = createDOMElement('legend', style);
-
-    legend.textContent = labelText;
-
-    return legend;
-  }
-
-  var appendWidgets = window.App.Utils.appendWidgets;
-  var getAppenderOf = window.App.Utils.getAppenderOf;
-  var getStylerOf = window.App.Utils.getStylerOf;
-  var createDOMElement = window.App.Utils.createDOMElement;
+  var delegateTo = window.App.Utils.delegateTo;
 
   window.App.Widgets.Section = Section;
 
