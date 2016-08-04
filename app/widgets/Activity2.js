deae265792e91a93d1e1b9504ebe9ab6c6756a07
Vlad GURDIGA <gurdiga@gmail.com>
2016-08-04 15:20:40 +0300

    Activity2: has style and child widgets

--- app/widgets/Activity2.js
+++ app/widgets/Activity2.js
@@ -3,15 +3,39 @@
 
   function Activity2(labelText) {
     var container = createContainer(labelText);
+    var dateField = createDateField();
+
+    container.appendWidgets([dateField]);
 
     this.appendTo = delegateTo(container, 'appendTo');
+    this.appendWidgets = delegateTo(container, 'appendWidgets');
   }
 
   function createContainer(labelText) {
-    return new LabeledContainer(labelText);
+    var labelStyle = {
+      'font-size': '16px',
+      'font-weight': 'bold'
+    };
+    var container = new LabeledContainer(labelText);
+
+    container.setLabelStyle(labelStyle);
+
+    return container;
+  }
+
+  function createDateField() {
+    var dateField = new DateField();
+    var style = {
+      'width': '6.5em'
+    };
+
+    dateField.setStyle(style);
+
+    return dateField;
   }
 
   var LabeledContainer = window.App.Widgets.LabeledContainer;
+  var DateField = window.App.Widgets.DateField;
 
   var delegateTo = window.App.Utils.delegateTo;
 
--- test/widgets/Activity2Test.js
+++ test/widgets/Activity2Test.js
@@ -3,19 +3,41 @@ describe('Activity2', function() {
 
   var Activity2 = window.App.Widgets.Activity2;
 
-  var activity, labelText, domElement, label;
+  var activity, labelText, domElement, label, dateField;
 
   beforeEach(function() {
     labelText = 'Some activity';
     activity = new Activity2(labelText);
     domElement = getWidgetDOMElement(activity);
-    label = domElement.firstChild;
+    label = domElement.children[0];
+    dateField = domElement.children[1];
   });
 
   it('has the appropriate DOM structure', function() {
     assert.equal(domElement.getAttribute('widget-name'), 'LabeledContainer', 'has a container');
     assert.equal(label.textContent, labelText, 'has the appropriate label text');
+    assert.equal(dateField.tagName, 'DATE-FIELD', 'has a date field');
   });
+
+  it('has the appropriate label style', function() {
+    assert.equal(label.style.fontSize, '16px', 'has the appropriate font size');
+    assert.equal(label.style.fontWeight, 'bold', 'has the appropriate font weight');
+
+    var dateInput = dateField.firstChild;
+    assert.equal(dateInput.style.width, '6.5em', 'has the appropriate width for the date input');
+  });
+
+  it('can be asked to appendWidgets', function() {
+    var childWidgets = [
+      new LabeledTextField('I am a text field')
+    ];
+    var childWidgetsDOMElements = childWidgets.map(getWidgetDOMElement);
+
+    activity.appendWidgets(childWidgets);
+    assert.equal(domElement.children[2], childWidgetsDOMElements[0], 'contains the appended widget');
+  });
+
+  var LabeledTextField = window.App.Widgets.LabeledTextField;
 
   var assert = window.TestHelpers.assert;
   var getWidgetDOMElement = window.TestHelpers.getWidgetDOMElement;
