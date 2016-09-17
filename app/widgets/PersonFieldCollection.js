47f24514c949e3b0457b3bcbcf99d4d71dfdfe39
Vlad GURDIGA <gurdiga@gmail.com>
2016-09-17 11:23:04 +0300

    PersonFieldCollection: works

--- app/widgets/PersonFieldCollection.js
+++ app/widgets/PersonFieldCollection.js
@@ -1,14 +1,31 @@
 (function() {
   'use strict';
 
-  function PersonFieldCollection() {
+  function PersonFieldCollection(fieldValues) {
     var domElement = createDOMElement('person-field-collection');
+    var fields = [
+      createField(LabeledTextField, 'Nume', 'nume', fieldValues),
+      createField(LabeledTextField, 'IDNP', 'idnp', fieldValues),
+      createField(LabeledDateField, 'Data nașterii', 'data-nașterii', fieldValues),
+      createField(LabeledLargeTextField, 'Domiciliu', 'domiciliu', fieldValues),
+      createField(LabeledLargeTextField, 'Note', 'note', fieldValues)
+    ];
+
+    appendWidgets(fields).to(domElement);
 
     this.appendTo = getAppenderOf(domElement);
+    this.getFieldValues = getFieldValueCollector(fields);
   }
+
+  var LabeledTextField = window.App.Widgets.LabeledTextField;
+  var LabeledLargeTextField = window.App.Widgets.LabeledLargeTextField;
+  var LabeledDateField = window.App.Widgets.LabeledDateField;
 
   var getAppenderOf = window.App.Utils.getAppenderOf;
   var createDOMElement = window.App.Utils.createDOMElement;
+  var createField = window.App.Utils.createField;
+  var appendWidgets = window.App.Utils.appendWidgets;
+  var getFieldValueCollector = window.App.Utils.getFieldValueCollector;
 
   window.App.Widgets.PersonFieldCollection = PersonFieldCollection;
 
--- test/widgets/PersonFieldCollectionTest.js
+++ test/widgets/PersonFieldCollectionTest.js
@@ -2,15 +2,62 @@ describe('PersonFieldCollection', function() {
   'use strict';
 
   var PersonFieldCollection = window.App.Widgets.PersonFieldCollection;
-  var personFieldCollection, domElement;
+  var personFieldCollection, defaultFieldValues, domElement;
 
-  beforeEach(function() {
-    personFieldCollection = new PersonFieldCollection();
+  before(function() {
+    defaultFieldValues = {
+      nume: 'John Doe',
+      idnp: '0123456789',
+      'data-nașterii': '17.02.2000',
+      domiciliu: 'Happiness str. 51',
+      note: 'Yes.'
+    };
+    personFieldCollection = new PersonFieldCollection(defaultFieldValues);
     domElement = getWidgetDOMElement(personFieldCollection);
   });
 
   it('has the appropriate DOM structure', function() {
     assert.equal(domElement.tagName, 'PERSON-FIELD-COLLECTION', 'tag name');
+  });
+
+  it('has the appropriate fields', function() {
+    var fields = domElement.childNodes;
+
+    assert.equal(fields.length, 5, 'has the appropriate number of fields');
+    assert.equal(fields[0].tagName, 'LABELED-TEXT-FIELD', 'has the appropriate type of fields');
+    assert.equal(fields[1].tagName, 'LABELED-TEXT-FIELD', 'has the appropriate type of fields');
+    assert.equal(fields[2].tagName, 'LABELED-DATE-FIELD', 'has the appropriate type of fields');
+    assert.equal(fields[3].tagName, 'LABELED-LARGE-TEXT-FIELD', 'has the appropriate type of fields');
+    assert.equal(fields[4].tagName, 'LABELED-LARGE-TEXT-FIELD', 'has the appropriate type of fields');
+  });
+
+  it('fields have the appropriate labels', function() {
+    var fields = domElement.childNodes;
+
+    assert.equal(fields[0].textContent, 'Nume');
+    assert.equal(fields[1].textContent, 'IDNP');
+    assert.equal(fields[2].textContent, 'Data nașterii');
+    assert.equal(fields[3].textContent, 'Domiciliu');
+    assert.equal(fields[4].textContent, 'Note');
+  });
+
+  it('fields have the appropriate default values', function() {
+    var fields = domElement.childNodes;
+
+    assert.equal(fields[0].querySelector('input').value, defaultFieldValues['nume']);
+    assert.equal(fields[1].querySelector('input').value, defaultFieldValues['idnp']);
+    assert.equal(fields[2].querySelector('input').value, defaultFieldValues['data-nașterii']);
+    assert.equal(fields[3].querySelector('textarea').value, defaultFieldValues['domiciliu']);
+    assert.equal(fields[4].querySelector('textarea').value, defaultFieldValues['note']);
+  });
+
+  it('can tell the field values', function() {
+    var fieldValues = personFieldCollection.getFieldValues();
+    var fieldNames = Object.keys(defaultFieldValues);
+
+    fieldNames.forEach(function(fieldName) {
+      assert.equal(fieldValues[fieldName], defaultFieldValues[fieldName], fieldName);
+    });
   });
 
   var assert = window.TestHelpers.assert;
